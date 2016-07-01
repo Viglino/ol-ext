0bfd771d9be4577873dd865720a604bc421cae82
Jean-Marc Viglino <jean-marc.viglino@ign.fr>
2016-07-01 15:49:39 +0200

    [ADD] color filter

---
+++ filter/colorizefilter.js
@@ -0,0 +1,89 @@
+/*	Copyright (c) 2016 Jean-Marc VIGLINO, 
+	released under the CeCILL-B license (French BSD license)
+	(http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt).
+*/
+/** Colorize map or layer
+*	@requires ol.filter
+*	@extends {ol.filter.Base}
+*	@param {ol.filter.cropOptions}
+*		- feature {ol.Feature} feature to mask with
+*		- fill {ol.style.Fill} style to fill with
+*		- inner {bool} mask inner, default false
+*/
+ol.filter.Colorize = function(options)
+{	ol.filter.Base.call(this, options);
+
+	this.setFilter(options);
+}
+ol.inherits(ol.filter.Colorize, ol.filter.Base);
+
+ol.filter.Colorize.prototype.setFilter = function(options)
+{	options = options || {};
+	switch (options)
+	{	case "grayscale": options = { operation:'hue', red:0, green:0, blue:0, value:1 }; break;
+		case "invert": options = { operation:'difference', red:255, green:255, blue:255, value:1 }; break;
+		case "sepia": options = { operation:'color', red:153, green:102, blue:51, value:0.6 }; break;
+		default: break;
+	}
+	this.set ('color', ol.color.asString([ options.red, options.green, options.blue, options.value]));
+	this.set ('value', options.value||1);
+	switch (options.operation)
+	{	case 'color':
+		case 'hue':
+		case 'difference':
+		case 'color-dodge':
+		case 'enhance':
+			this.set ('operation', options.operation);
+			break;
+		case 'saturation':
+			var v = 255*(options.value || 0);
+			this.set('color', ol.color.asString([0,0,v,v||1]));
+			this.set ('operation', options.operation);
+			break;
+		case 'luminosity':
+			var v = 255*(options.value || 0);
+			this.set('color', ol.color.asString([v,v,v,255]));
+			//this.set ('operation', 'luminosity')
+			this.set ('operation', 'hard-light');
+			break;
+		default: 
+			this.set ('operation', 'color');
+			break;
+	}
+}
+
+ol.filter.Colorize.prototype.precompose = function(e)
+{	// Colorize using vector features of the layer
+	/*
+	if (e.target.getSource && e.target.getSource().getFeatures)
+	{	ctx.save();
+		ctx.globalCompositeOperation = this.get('operation');
+	}
+	*/
+}
+
+ol.filter.Colorize.prototype.postcompose = function(e)
+{	// Set back color hue
+	var ctx = e.context;
+	var canvas = ctx.canvas;
+	
+	ctx.save();
+		if (this.get('operation')=='enhance')
+		{	var v = this.get('value');
+			if (v)
+			{	var w = canvas.width;
+				var h = canvas.height;
+				ctx.globalCompositeOperation = 'color-burn'
+				ctx.globalAlpha = v;
+				ctx.drawImage (canvas, 0, 0, w, h);
+				ctx.drawImage (canvas, 0, 0, w, h);
+				ctx.drawImage (canvas, 0, 0, w, h);
+			}
+		}
+		else
+		{	ctx.globalCompositeOperation = this.get('operation');
+			ctx.fillStyle = this.get('color');
+			ctx.fillRect(0,0,canvas.width,canvas.height);  
+		}
+	ctx.restore();
+}
