2c5e9f211bf1ea7db83e39ca049ec02bed2186f4
Jean-Marc Viglino <jean-marc.viglino@ign.fr>
2016-08-21 10:38:45 +0200

    [ADD] Submenus

--- control/controlbar.js
+++ control/controlbar.js
@@ -5,7 +5,7 @@
  * @extends {ol.control.Control}
  * @param {Object=} opt_options Control options.
  *		className {String} class of the control
- *		group {bool} the is a group, default false
+ *		group {bool} is a group, default false
  *		toggleOne {bool} only one toggle control is active at a time, default false
  *		controls {Array<ol.control>} a list of control to add to the bar
  */
@@ -70,13 +70,21 @@ ol.control.Bar.prototype.setPosition = function (pos)
 }
 
 /** Add a control to the bar
-*	@param {ol.control}
+*	@param {ol.control} c control to add
+*	@param {ol.control.Bar} bar an option bar essociated with the control (drawn when active)
 */
-ol.control.Bar.prototype.addControl = function (c)
+ol.control.Bar.prototype.addControl = function (c, bar)
 {	this.controls_.push(c);
 	c.setTarget(this.element);
+	if (bar)
+	{	this.controls_.push(bar);
+		bar.setTarget(c.element);
+		$(bar.element).addClass("ol-option-bar");
+		c.option_bar = bar;
+	}
 	if (this.getMap()) 
 	{	this.getMap().addControl(c);
+		if (c.option_bar) this.getMap().addControl(c.option_bar);
 		c.on ('change:active', this.onActivateControl_, this);
 	}
 	//$(this.element).append(c.element);
@@ -87,8 +95,8 @@ ol.control.Bar.prototype.addControl = function (c)
 */
 ol.control.Bar.prototype.onActivateControl_ = function (e)
 {	if (!e.active || !this.get('toggleOne')) return;
-	var ctrl = e.target;
 	var n;
+	var ctrl = e.target;
 	for (n=0; n<this.controls_.length; n++) 
 	{	if (this.controls_[n]===ctrl) break;
 	}
