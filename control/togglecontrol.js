544a829de0059c779250b2bd2182cc6f67afd6ed
Jean-Marc Viglino <jean-marc.viglino@ign.fr>
2016-08-28 19:03:15 +0200

    [ADD] Profil control

--- control/togglecontrol.js
+++ control/togglecontrol.js
@@ -1,3 +1,7 @@
+/*	Copyright (c) 2016 Jean-Marc VIGLINO, 
+	released under the CeCILL-B license (French BSD license)
+	(http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt).
+*/
 /** A simple toggle control 
  *
  * @constructor
