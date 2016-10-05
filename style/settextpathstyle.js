ee189830b80ae2eb8e710f79153fff31f5fd33d0
Jean-Marc Viglino <jean-marc.viglino@ign.fr>
2016-10-05 16:34:27 +0200

    [ADD] textpath style to draw text along a path

---
+++ style/settextpathstyle.js
@@ -0,0 +1,181 @@
+/** Add a setTextPath style to draw text along linestrings
+*/
+(function()
+{
+
+function drawTextPath (e)
+{
+	var extent = e.frameState.extent;
+	var c2p = e.frameState.coordinateToPixelTransform;
+	var ctx = e.context;
+
+	// Get pixel path with coordinates
+	function getPath(c, readable)
+	{	var path1 = [];
+		for (var k=0; k<c.length; k++) 
+		{	path1.push(c2p[0]*c[k][0]+c2p[1]*c[k][1]+c2p[4]);
+			path1.push(c2p[2]*c[k][0]+c2p[3]*c[k][1]+c2p[5]);
+		}
+		// Revert line ?
+		if (readable && path1[0]>path1[path1.length-2])
+		{	var path2 = [];
+			for (var k=path1.length-2; k>=0; k-=2)
+			{	path2.push(path1[k]);
+				path2.push(path1[k+1]);
+			}
+			return path2;
+		}
+		else return path1;
+	}
+
+	ctx.save();
+	ctx.scale(e.frameState.pixelRatio,e.frameState.pixelRatio);
+
+	var features = this.getSource().getFeaturesInExtent(extent);
+	for (var i=0, f; f=features[i]; i++)
+	{	{	var style = this.textPathStyle_(f,e.frameState.viewState.resolution);
+			for (var s,j=0; s=style[j]; j++)
+			{	
+				var g = s.getGeometry() || f.getGeometry();
+				var c;
+				switch (g.getType())
+				{	case "LineString": c = g.getCoordinates(); break;
+					case "ol.geom.MultiLineString": c = g.getLineString(0).getCoordinates(); break;
+					default: continue;
+				}
+
+				var st = s.getText();
+				var path = getPath(c, st.getRotateWithView() );
+				
+				ctx.font = st.getFont();
+				ctx.textBaseline = st.getTextBaseline();
+				ctx.textAlign = st.getTextAlign();
+				ctx.lineWidth = st.getStroke() ? (st.getStroke().getWidth()||0) : 0;
+				ctx.strokeStyle = st.getStroke() ? (st.getStroke().getColor()||"#fff") : "#fff";
+				ctx.fillStyle = st.getFill() ? st.getFill().getColor()||"#000" : "#000";
+				// Draw textpath
+				ctx.textPath(st.getText()||f.get("name"), path);
+			}
+		}
+	}
+
+	ctx.restore();
+}
+
+/** Set the style for features. 
+*	This can be a single style object, an array of styles, or a function that takes a feature and resolution and 
+*	returns an array of styles. If it is undefined the default style is used. 
+*	If it is null the layer has no style (a null style). 
+*	See ol.style for information on the default style.
+*	@param {ol.style.Style|Array.<ol.style.Style>|ol.StyleFunction} style
+*/
+ol.layer.Vector.prototype.setTextPathStyle = function(style)
+{
+	// Remove existing style
+	if (style===null)
+	{	if (this.textPath_) this.unByKey(this.textPath_);
+		this.textPath_ = null;
+		this.changed();
+		return;
+	}
+	// New postcompose
+	if (!this.textPath_)
+	{	this.textPath_ = this.on('postcompose', drawTextPath, this);
+	}
+	// Set textPathStyle
+	if (style===undefined)
+	{	style = [ new ol.style.Style({ text: new ol.style.Text()}) ];
+	}
+	if (typeof(style) == "function") this.textPathStyle_ = style;
+	else this.textPathStyle_ = function() { return style; };
+
+	this.changed();
+
+}
+
+})();
+
+
+/** Draw text along path
+* @param {string} text
+* @param {Array<Number>} path
+*/
+CanvasRenderingContext2D.prototype.textPath = function (text, path)
+{
+	var ctx = this;
+
+	function dist2D(x1,y1,x2,y2)
+	{	var dx = x2-x1;
+		var dy = y2-y1;
+		return Math.sqrt(dx*dx+dy*dy);
+	}
+  
+	var di, dpos=0;
+	var pos=2;
+	function getPoint(path, dl)
+	{	if (!di || dpos+di<dl)
+		{ for (; pos<path.length; )
+			{	di = dist2D(path[pos-2],path[pos-1],path[pos],path[pos+1]);
+				if (dpos+di>dl) break;
+				pos += 2;
+				if (pos>=path.length) break;
+				dpos += di;
+			}
+		}
+   
+		var x, y, a, dt = dl-dpos;
+		if (pos>=path.length) 
+		{	pos = path.length-2;
+		}
+
+		if (!dt) 
+		{	x = path[pos-2];
+			y = path[pos-1];
+			a = Math.atan2(path[pos+1]-path[pos-1], path[pos]-path[pos-2]);
+		}
+		else
+		{	x = path[pos-2]+ (path[pos]-path[pos-2])*dt/di;
+			y = path[pos-1]+(path[pos+1]-path[pos-1])*dt/di;
+			a = Math.atan2(path[pos+1]-path[pos-1], path[pos]-path[pos-2]);
+		}
+		return [x,y,a];
+	}
+
+	var letterPadding = ctx.measureText(" ").width *0.25;
+	var w = text.length;
+	var ww = ctx.measureText(text).width;
+  
+	var start = 0;
+	switch (ctx.textAlign)
+	{	case "center":
+		case "end":
+		case "right":
+		{	var d = 0;
+			for (var i=2; i<path.length; i+=2)
+			{	d += dist2D(path[i-2],path[i-1],path[i],path[i+1])
+			}
+			var nbspace = text.split(" ").length -1;
+			start = d - ctx.measureText(text).width - text.length*letterPadding - nbspace*letterPadding;
+			if (ctx.textAlign == "center") start /= 2;
+			break;
+		}
+		default: break;
+	}
+  
+	for (var t=0; t<text.length; t++)
+	{	var letter = text[t];
+		var wl = ctx.measureText(letter).width;
+    
+		var p = getPoint(path, start+wl/2);
+
+		ctx.save();
+		ctx.textAlign = "center";
+		ctx.translate(p[0], p[1]);
+		ctx.rotate(p[2]);
+		if (ctx.lineWidth) ctx.strokeText(letter,0,0);
+		ctx.fillText(letter,0,0);
+		ctx.restore();
+		start += wl+letterPadding*(letter==" "?2:1);
+	}
+  
+};
