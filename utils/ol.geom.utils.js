8d3d9811da8994d4be8b7d538f045086c4fe962f
Jean-Marc Viglino <jean-marc.viglino@ign.fr>
2016-10-23 15:08:03 +0200

    [ADD] usfull geom functions

---
+++ utils/ol.geom.utils.js
@@ -0,0 +1,88 @@
+﻿/*	Copyright (c) 2016 Jean-Marc VIGLINO, 
+	released under the CeCILL-B license (French BSD license)
+	(http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt).
+
+	Usefull function to handle geometric operations
+*/
+
+
+/** Distance beetween 2 points
+*	Usefull geometric functions
+*/
+ol.coordinate.dist2d = function(p1, p2)
+{	var dx = p1[0]-p2[0];
+	var dy = p1[1]-p2[1];
+	return Math.sqrt(dx*dx+dy*dy);
+}
+/** 2 points are equal
+*	Usefull geometric functions
+*/
+ol.coordinate.equal = function(p1, p2)
+{	return (p1[0]==p2[0] && p1[1]==p2[1]);
+}
+
+/** Split a lineString by a point or a list of points
+*	NB: points must be on the line, use getClosestPoint() to get one
+* @param {ol.Coordinate | Array<ol.Coordinate>} pt points to split the line
+* @param {Number} tol distance tolerance for 2 points to be equal
+*/
+ol.geom.LineString.prototype.splitAt = function(pt, tol)
+{	if (!pt) return [this];
+	if (!tol) tol = 1e-10;
+	// Test if list of points
+	if (pt.length && pt[0].length)
+	{	var result = [this];
+		for (var i=0; i<pt.length; i++)
+		{	var r = [];
+			for (var k=0; k<result.length; k++)
+			{	var ri = result[k].splitAt(pt[i]);
+				r = r.concat(ri);
+			}
+			result = r;
+		}
+		return result;
+	}
+	// Nothing to do
+	if (ol.coordinate.equal(pt,this.getFirstCoordinate())
+	 || ol.coordinate.equal(pt,this.getLastCoordinate())) 
+	{	return [this];
+	}
+	// Get 
+	var c0 = this.getCoordinates();
+	var c1=[c0[0]], c2=[], p0, p1;
+	var c = c1;
+	for (var i=0; i<c0.length-1; i++)
+	{	// Filter equal points
+		if (ol.coordinate.equal(c0[i],c0[i+1])) continue;
+		// Test end of first part
+		if (c!==c2)
+		{	// Extremity found  
+			if (ol.coordinate.equal(pt,c0[i+1]))
+			{	c.push(c0[i+1]);
+				c = c2;
+			}
+			// Test alignement
+			else
+			{	var d1, d2;
+				if (c0[i][0] == c0[i+1][0])
+				{	d1 = d2 = (c0[i][1]-pt[1]) / (c0[i][1]-c0[i+1][1]);
+				}
+				else if (c0[i][1] == c0[i+1][1])
+				{	d1 = d2 = (c0[i][0]-pt[0]) / (c0[i][0]-c0[i+1][0]);
+				}
+				else
+				{	d1 = (c0[i][0]-pt[0]) / (c0[i][0]-c0[i+1][0]);
+					d2 = (c0[i][1]-pt[1]) / (c0[i][1]-c0[i+1][1]);
+				}
+				if (Math.abs(d1-d2)<tol && 0<=d1 && d1<=1)
+				{	c.push(pt);
+					c = c2;
+					c.push(pt);
+				}
+			}
+		}
+		c.push(c0[i+1]);
+	}
+	if (c2.length) return [new ol.geom.LineString(c1), new ol.geom.LineString(c2)];
+	else return [this];
+}
