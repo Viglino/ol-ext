05a3da873cc67c7fff1b80ed6de9e104b3c49ae8
Jean-Marc Viglino <jean-marc.viglino@ign.fr>
2016-11-06 20:22:17 +0100

    [ADD] ol.control.Button

---
+++ control/buttoncontrol.js
@@ -0,0 +1,35 @@
+/*	Copyright (c) 2016 Jean-Marc VIGLINO, 
+	released under the CeCILL-B license (French BSD license)
+	(http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt).
+*/
+/** A simple button control 
+ *
+ * @constructor
+ * @extends {ol.control.Control}
+ * @param {Object=} opt_options Control options.
+ *		className {String} class of the control
+ *		title {String} title of the control
+ *		html {String} html to insert in the control
+ *		handleClick {function} callback when control is clicked (or use change:active event)
+ */
+ol.control.Button = function(options) 
+{	options = options || {};
+	var element = $("<div>").addClass(options.className + ' ol-button ol-unselectable ol-control');
+	var self = this;
+
+	$("<button>").html(options.html || "")
+				.attr('title', options.title)
+				.on("touchstart click", function(e)
+				{	if (e && e.preventDefault) e.preventDefault();
+					if (options.handleClick) options.handleClick.call(self, e);
+				})
+				.appendTo(element);
+	
+	ol.control.Control.call(this, 
+	{	element: element.get(0),
+		target: options.target
+	});
+
+	if (options.title) this.set("title", options.title);
+}
+ol.inherits(ol.control.Button, ol.control.Control);
