0b8429e97f18c3e7fed9588edff66e48dd8b1829
Jean-Marc Viglino <jean-marc.viglino@ign.fr>
2017-05-14 16:43:00 +0200

    [ADD] control graticule

--- control/graticulecontrol.js
+++ control/graticulecontrol.js
@@ -7,12 +7,12 @@
  *
  * @constructor
  * @extends {ol.control.Control}
- * @param {Object=} Control options. The style {ol.style.Style} option is usesd to draw the text.
- *	- className {string} class name for the control
- *	- image {Image} an image, default use the src option or a default image
- *	- src {string} image src, default use the image option or a default image
- *	- rotateVithView {boolean} rotate vith view (false to show watermark), default true
- *	- style {ol.style.Stroke} style to draw the lines, default draw no lines
+ * @param {Object=} Control options. 
+ *	- maxResolution {number} max resolution to display the graticule
+ *	- strokeStyle {ol.style.Stroke} The stroke style to use for drawing the graticule, default black.
+ *	- step {number} 
+ *	- delta {number} , default 40px 
+ *	- projection {ol.projectionLike} projection to use for the graticule, default EPSG:4326 
  */
 ol.control.Graticule = function(options) 
 {	var self = this;
@@ -24,11 +24,16 @@ ol.control.Graticule = function(options)
 	
 	ol.control.Control.call(this, { element: elt });
 
-	this.set('maxResolution', options.maxResolution);
+	this.set('projection', options.projection || 'EPSG:4326');
+	this.set('projection', options.projection || 'EPSG:2154');
+//	this.set('projection', options.projection || 'EPSG:27700');
 
-	this.proj = 'EPSG:4326';
-//	this.proj = 'EPSG:2154';
-//	this.proj = 'EPSG:27700';
+	var p = new ol.proj.Projection({code:this.get('projection')});
+
+	this.set('maxResolution', options.maxResolution);
+	this.set('step', options.step || 1);
+	this.set('delta', options.delta || 40);
+
 };
 ol.inherits(ol.control.Graticule, ol.control.Control);
 
@@ -56,6 +61,8 @@ ol.control.Graticule.prototype.drawGraticule_ = function (e)
 	var w = canvas.width/ratio;
 	var h = canvas.height/ratio;
 
+	var proj = this.get('projection');
+
 	var map = this.getMap();
 	var bbox = 
 	[	map.getCoordinateFromPixel([0,0]),
@@ -68,15 +75,15 @@ ol.control.Graticule.prototype.drawGraticule_ = function (e)
 	var ymax = -Infinity;
 	var ymin = Infinity;
 	for (var i=0, c; c=bbox[i]; i++)
-	{	bbox[i] = ol.proj.transform (c, map.getView().getProjection(), this.proj);
+	{	bbox[i] = ol.proj.transform (c, map.getView().getProjection(), proj);
 		xmax = Math.max (xmax, bbox[i][0]);
 		xmin = Math.min (xmin, bbox[i][0]);
 		ymax = Math.max (ymax, bbox[i][1]);
 		ymin = Math.min (ymin, bbox[i][1]);
 	}
 
-	var delta = 50;
-	var step = 0.1;
+	var delta = this.get('delta');
+	var step = this.get('step');
 
 	// Limit max line draw
 	var ds = (xmax-xmin)/step*delta;
@@ -95,21 +102,21 @@ ol.control.Graticule.prototype.drawGraticule_ = function (e)
 		ctx.beginPath();
 
 		for (var x=xmin; x<xmax; x += step)
-		{	var p0 = ol.proj.transform ([x, ymin], this.proj, map.getView().getProjection());
+		{	var p0 = ol.proj.transform ([x, ymin], proj, map.getView().getProjection());
 			p0 = map.getPixelFromCoordinate(p0);
 			ctx.moveTo(p0[0], p0[1]);
 			for (var y=ymin+step; y<=ymax; y+=step)
-			{	var p1 = ol.proj.transform ([x, y], this.proj, map.getView().getProjection());
+			{	var p1 = ol.proj.transform ([x, y], proj, map.getView().getProjection());
 				p1 = map.getPixelFromCoordinate(p1);
 				ctx.lineTo(p1[0], p1[1]);
 			}
 		}
 		for (var y=ymin; y<ymax; y += step)
-		{	var p0 = ol.proj.transform ([xmin, y], this.proj, map.getView().getProjection());
+		{	var p0 = ol.proj.transform ([xmin, y], proj, map.getView().getProjection());
 			p0 = map.getPixelFromCoordinate(p0);
 			ctx.moveTo(p0[0], p0[1]);
 			for (var x=xmin+step; x<=xmax; x+=step)
-			{	var p1 = ol.proj.transform ([x, y], this.proj, map.getView().getProjection());
+			{	var p1 = ol.proj.transform ([x, y], proj, map.getView().getProjection());
 				p1 = map.getPixelFromCoordinate(p1);
 				ctx.lineTo(p1[0], p1[1]);
 			}
