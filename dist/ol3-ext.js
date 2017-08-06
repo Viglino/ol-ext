7cb63866d1705a2e5f4c4fa246925e3b6870e0bf
Jean-Marc Viglino <jean-marc.viglino@ign.fr>
2017-08-06 18:54:34 +0200

    [UPD] generic search control

--- dist/ol3-ext.js
+++ dist/ol3-ext.js
@@ -3682,13 +3682,14 @@ ol.control.Profil.prototype.getImage = function(type, encoderOptions)
  * @param {Object=} Control options. 
  *	- className {string} control class name
  *	- target {Element | string | undefined} Specify a target if you want the control to be rendered outside of the map's viewport.
- *	- placeholder {string | undefined} placeholder, default "search..."
+ *	- placeholder {string | undefined} placeholder, default "Search..."
  *	- typing {number | undefined} a delay on each typing to start searching (ms), default 300.
  *	- source {ol.source.Vector} source to search in
- *	- property {string | function | undefined} a property to display in the index or a function that takes a feature and return the name to display in the index, default 'name'.
- *	- getSearchString {function | undefined} a function that take a feature and return a text to be used as search string
  *	- minLength {integer | undefined} minimum length to start searching, default 1
- *	- maxItems {integer | undefined} maximum number of items to display in the list, default 10
+ *	- maxItems {integer | undefined} maximum number of items to display in the autocomplete list, default 10
+*
+ *	- property {string | function | undefined} a property to display in the index or a function that takes a feature and return the name to display in the index, default 'name'.
+ *	- getSearchString {function | undefined} a function that take a feature and return a text to be used as search string, default property is used as seach string
  */
 ol.control.SearchFeature = function(options) 
 {	var self = this;
@@ -3704,21 +3705,54 @@ ol.control.SearchFeature = function(options)
 					.attr('type','button')
 					.click (function()
 					{	element.toggleClass("ol-collapsed"); 
-						if (!element.hasClass("ol-collapsed")) $("input", element).focus();
+						if (!element.hasClass("ol-collapsed")) 
+						{	$("input", element).focus();
+							$('li', element).removeClass('select');
+						}
 					})
 					.appendTo(element);
 	}
 	// Search input
 	var tout, cur="";
 	$("<input>").attr('type','search')
-		.attr('placeholder', options.placeholder||"search...")
+		.attr('placeholder', options.placeholder||"Search...")
 		.on('keyup search', function(e) 
-		{	if (cur != $(this).val())
-			{	cur = $(this).val();
+		{	if (e.key=='ArrowDown' || e.key=='ArrowUp')
+			{	var i, li  = $("li", element);
+				for (i=0; i<li.length; i++) if ($(li[i]).hasClass('select')) break;
+				if (i<li.length)
+				{	var l = $(li[i+(e.key=='ArrowDown' ? 1 : -1)]);
+					if (l.length) 
+					{	$(li[i]).removeClass('select');
+						l.addClass('select');
+					}
+				}
+				else $(li[0]).addClass('select');
+			}
+			else if (e.key =='Enter')
+			{	var i, li  = $("li", element);
+				for (i=0; i<li.length; i++) if ($(li[i]).hasClass('select')) break;
+				if (i<li.length) 
+				{	$(this).blur();
+					self.dispatchEvent({ type:"select", feature:$(li[i]).data('feature') });
+				}
+				else 
+				{	self.search($(this).val(), function(f)
+					{	self.dispatchEvent({ type:"select", feature:f });
+					});
+				}
+			}
+			else if (cur != $(this).val())
+			{	// current search
+				cur = $(this).val();
 				// prevent searching on each typing
 				if (tout) clearTimeout(tout);
-				tout = setTimeout(function(){ self.search(cur)}, options.typing || 300);
+				tout = setTimeout(function()
+				{	if (cur.length >= self.get("minLength")) self.autocomplete_(cur);
+					else $("ul", this.element).html("");
+				}, options.typing || 300);
 			}
+			else $("li", element).removeClass('select');
 		})
 		.blur(function()
 		{	setTimeout(function(){ element.addClass('ol-collapsed') }, 200);
@@ -3768,12 +3802,31 @@ ol.control.SearchFeature.prototype.getSearchString = function (f)
 
 /** Search a string in the features
 *	@param {string} s the search string
+*	@private
 */
-ol.control.SearchFeature.prototype.search = function (s)
+ol.control.SearchFeature.prototype.autocomplete_ = function (s)
 {	var ul = $("ul", this.element).html("");
-	if (s.length < this.get("minLength")) return;
 	var self = this;
-	var nb = this.get("maxItems");
+	this.autocomplete (s, this.get("maxItems"), function(auto)
+	{	for (var i=0; i<auto.length; i++)
+		{	$("<li>").text(auto[i].name)
+				.data('feature', auto[i].feature)
+				.click(function(e)
+				{	self.dispatchEvent({ type:"select", feature:$(this).data('feature') });
+				})
+				.appendTo(ul);
+		}
+	});
+};
+
+/** Autocomplete function
+* @param {string} s search string
+* @param {int} max max 
+* @param {function} cback a callback function that takes an array of {name, feature} to display in the autocomplete fielad
+* @api
+*/
+ol.control.SearchFeature.prototype.autocomplete = function (s, max, cback)
+{	var result = [];
 	// regexp
 	s = s.replace(/^\*/,'');
 	var rex = new RegExp(s, 'i');
@@ -3781,15 +3834,20 @@ ol.control.SearchFeature.prototype.search = function (s)
 	var features = this.source_.getFeatures();
 	for (var i=0, f; f=features[i]; i++)
 	{	if (rex.test(this.getSearchString(f)))
-		{	$("<li>").text(this.getFeatureName(f))
-					.data('feature', f)
-					.click(function(e)
-					{	self.dispatchEvent({ type:"select", feature:$(this).data('feature') });
-					})
-					.appendTo(ul);
-			if ((--nb)<=0) break;;
+		{	result.push({ name:this.getFeatureName(f), feature:f });
+			if ((--max)<=0) break;
 		}
 	}
+	cback (result);
+};
+
+/** Search a feature 
+* @param {string} s search string
+* @param {function} cback a callback function that takes a feature
+* @api
+*/
+ol.control.SearchFeature.prototype.search = function (s, cback)
+{	
 };
 
 /*	Copyright (c) 2015 Jean-Marc VIGLINO, 
