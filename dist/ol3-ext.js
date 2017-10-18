29a1f82b8f5b15ec663f7fcb8a9bbd36f06ef0d2
Jean-Marc Viglino <jean-marc.viglino@ign.fr>
2017-10-18 10:37:33 +0200

    [UPD] remove jQuery dependencies for ol.control.GeoBookmark

--- dist/ol3-ext.js
+++ dist/ol3-ext.js
@@ -1800,133 +1800,161 @@ ol.control.Gauge.prototype.val = function(v)
  * @extends {ol.control.Control}
  * @trigger add|remove when a bookmark us added or deleted
  * @param {Object=} Control options.
- *	- className {string} default ol-bookmark
- *	- placeholder {string} input placeholder, default Add a new geomark...
- *	- editable {bool} enable modification, default true
+ *  - className {string} default ol-bookmark
+ *  - placeholder {string} input placeholder, default Add a new geomark...
+ *  - editable {bool} enable modification, default true
  *  - marks a list of default bookmarks : { BM1:{pos:ol.coordinates, zoom: integer, permanent: true}, BM2:{pos:ol.coordinates, zoom: integer} }
  */
-ol.control.GeoBookmark = function(options) 
-{	options = options || {};
-	var self = this;
+ol.control.GeoBookmark = function(options) {
+  options = options || {};
+  var self = this;
 
-	var element;
-	if (options.target) 
-	{	element = $("<div>").addClass(options.className || "ol-bookmark");
-	}
-	else
-	{	element = $("<div>").addClass((options.className || 'ol-bookmark') +' ol-unselectable ol-control ol-collapsed')
-						.on("mouseleave", function()
-						{	if (!input.is(":focus")) menu.hide();
-						});
-		// Show bookmarks on clik
-		this.button = $("<button>")
-					.attr('type','button')
-					.click(function(e)
-					{	menu.toggle();
-					})
-					.appendTo(element);
-	}
-	// The menu
-	var menu = $("<div>")
-				.appendTo(element);
-	var ul = $("<ul>")
-				.appendTo(menu);
-	var input = $("<input>")
-			.attr('placeholder', options.placeholder || "Add a new geomark...")
-			.on('change', function(e)
-			{	var title = $(this).val();
-				if (title) 
-				{	self.addBookmark(title);
-					$(this).val("");
-					self.dispatchEvent({ type:"add", name: title })
-				}
-				menu.hide();
-			})
-			.on('blur', function(){ menu.hide(); });
-	if (options.editable!==false) input.appendTo(menu);
+  var element = document.createElement('div');
+  if (options.target) {
+    element.className = options.className || "ol-bookmark";
+  } else {
+    element.className = (options.className || "ol-bookmark") +
+          " ol-unselectable ol-control ol-collapsed";
+    element.addEventListener("mouseleave", function() {
+      if (input !== document.activeElement) {
+        menu.style.display = 'none';
+      };
+    });
+    // Show bookmarks on click
+    this.button = document.createElement('button');
+    this.button.setAttribute('type', 'button');
+    this.button.addEventListener('click', function(e) {
+      menu.style.display = (menu.style.display === '' || menu.style.display === 'none' ? 'block': 'none');
+    });
+    element.appendChild(this.button);
+  }
+  // The menu
+  var menu = document.createElement('div');
+  element.appendChild(menu);
+  var ul = document.createElement('ul');
+  menu.appendChild(ul);
+  var input = document.createElement('input');
+  input.setAttribute("placeholder", options.placeholder || "Add a new geomark...")
+  input.addEventListener("change", function(e) {
+    var title = this.value;
+    if (title) {
+      self.addBookmark(title);
+      this.value = '';
+      self.dispatchEvent({
+        type: "add",
+        name: title
+      });
+    }
+    menu.style.display = 'none';
+  });
+  input.addEventListener("blur", function() {
+    menu.style.display = 'none';
+  });
+  if (options.editable !== false) {
+    menu.appendChild(input);
+  };
 
-	// Init
-	ol.control.Control.call(this, 
-	{	element: element.get(0),
-		target: options.target
-	});
+  // Init
+  ol.control.Control.call(this, {
+    element: element,
+    target: options.target
+  });
 
-	this.set('editable', options.editable!==false);
-	// Set default bmark
-	this.setBookmarks(options.marks);
-}
+  this.set("editable", options.editable !== false);
+  // Set default bmark
+  this.setBookmarks(localStorage["ol@bookmark"] ? null:options.marks);
+};
 ol.inherits(ol.control.GeoBookmark, ol.control.Control);
 
-
 /** Set bookmarks
-* @param {} bmark a list of bookmarks : { BM1:{pos:ol.coordinates, zoom: integer}, BM2:{pos:ol.coordinates, zoom: integer} }
-* @param [boolean} modify, default false
+* @param {} bmark a list of bookmarks, default retreave in the localstorage
+*   : { "Mark 1":{pos:ol.coordinates, zoom: integer}, "Mark 2":{pos:ol.coordinates, zoom: integer} }
 */
-ol.control.GeoBookmark.prototype.setBookmarks = function (bmark)
-{	if (!bmark) bmark = JSON.parse(localStorage['ol@bookmark'] || "{}");
-	var modify = this.get('editable');
-	var ul = $("ul", this.element);
-	var menu = $("> div", this.element);
-	var self = this;
+ol.control.GeoBookmark.prototype.setBookmarks = function(bmark) {
+  if (!bmark) bmark = JSON.parse(localStorage["ol@bookmark"] || "{}");
+  var modify = this.get("editable");
+  var ul = this.element.querySelector("ul");
+  var menu = this.element.querySelector("div");
+  var self = this;
 
-	localStorage['ol@bookmark'] = JSON.stringify(bmark);
-	ul.html("");
-	for (var b in bmark)
-	{	var li = $("<li>").text(b)
-			.data('bookmark', bmark[b])
-			.click(function()
-			{	var bm = $(this).data('bookmark');
-				self.getMap().getView().setCenter(bm.pos);
-				self.getMap().getView().setZoom(bm.zoom);
-				menu.hide();
-			})
-		li.appendTo(ul);
-		if (modify && !bmark[b].permanent)
-		{	$("<button>")
-				.data('name', b)
-				.attr('title', "Suppr.")
-				.click(function(e)
-				{	self.removeBookmark($(this).data('name'));
-					self.dispatchEvent({ type:"add", name: $(this).data('name') })
-					e.stopPropagation()
-				})
-				.appendTo(li);
-		}
-	}
+  ul.innerHTML = '';
+  for (var b in bmark) {
+    var li = document.createElement('li');
+    li.textContent = b;
+    li.setAttribute('data-bookmark', JSON.stringify(bmark[b]));
+    li.addEventListener('click', function() {
+      var bm = JSON.parse(this.getAttribute("data-bookmark"));
+      self
+        .getMap()
+        .getView()
+        .setCenter(bm.pos);
+      self
+        .getMap()
+        .getView()
+        .setZoom(bm.zoom);
+      menu.style.display = 'none';
+    });
+    ul.appendChild(li);
+    if (modify && !bmark[b].permanent) {
+      var button = document.createElement('button');
+      button.setAttribute('data-name', b);
+      button.setAttribute("title", "Suppr.");
+      button.addEventListener('click', function(e) {
+        self.removeBookmark(this.getAttribute("data-name"));
+        self.dispatchEvent({ type: "add", name: this.getAttribute("data-name") });
+        e.stopPropagation();
+      });
+      li.appendChild(button);
+    }
+  }
+  localStorage["ol@bookmark"] = JSON.stringify(bmark);
 };
 
 /** Get Geo bookmarks
 * @return a list of bookmarks : { BM1:{pos:ol.coordinates, zoom: integer}, BM2:{pos:ol.coordinates, zoom: integer} }
 */
-ol.control.GeoBookmark.prototype.getBookmarks = function ()
-{	return JSON.parse(localStorage['ol@bookmark'] || "{}");
+ol.control.GeoBookmark.prototype.getBookmarks = function() {
+  return JSON.parse(localStorage["ol@bookmark"] || "{}");
 };
 
 /** Remove a Geo bookmark
 * @param {string} name
 */
-ol.control.GeoBookmark.prototype.removeBookmark = function (name)
-{	if (!name) return;
-	bmark = JSON.parse(localStorage['ol@bookmark'] || "{}");
-	delete bmark[name];
-	this.setBookmarks(bmark);
+ol.control.GeoBookmark.prototype.removeBookmark = function(name) {
+  if (!name) {
+    return;
+  };
+  bmark = JSON.parse(localStorage["ol@bookmark"] || "{}");
+  delete bmark[name];
+  this.setBookmarks(bmark);
 };
 
-/** Add a new Geo bookmark
-* @param {string} name
+/** Add a new Geo bookmark (replace existing one if any)
+* @param {string} name name of the bookmark (display in the menu)
 * @param {ol.Coordintes} position, default current position
-* @param {number} zoom, default default map zoom
+* @param {number} zoom, default current map zoom
 * @param {bool} permanent: prevent from deletion, default false
 */
-ol.control.GeoBookmark.prototype.addBookmark = function (name, position, zoom, permanent)
-{	if (!name) return;
-	bmark = JSON.parse(localStorage['ol@bookmark'] || "{}");
-	bmark[name] = 
-		{	pos: position || this.getMap().getView().getCenter(), 
-			zoom: zoom || this.getMap().getView().getZoom()
-		};
-	if (permanent) bmark[name].permanent = true;
-	this.setBookmarks(bmark);
+ol.control.GeoBookmark.prototype.addBookmark = function(name, position, zoom, permanent) 
+{
+  if (!name) return;
+  bmark = JSON.parse(localStorage["ol@bookmark"] || "{}");
+  bmark[name] = {
+    pos:
+      position ||
+      this.getMap()
+        .getView()
+        .getCenter(),
+    zoom:
+      zoom ||
+      this.getMap()
+        .getView()
+        .getZoom()
+  };
+  if (permanent) {
+    bmark[name].permanent = true;
+  }
+  this.setBookmarks(bmark);
 };
 
 /*	Copyright (c) 2016 Jean-Marc VIGLINO, 
