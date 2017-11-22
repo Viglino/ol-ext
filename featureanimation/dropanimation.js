63cdf51aaee0c53d3a02d86d7dd6551689ac48ac
Jean-Marc Viglino <jean-marc.viglino@ign.fr>
2017-11-22 14:34:55 +0100

    [UPD] handle map rotation on drop #58

--- featureanimation/dropanimation.js
+++ featureanimation/dropanimation.js
@@ -21,13 +21,21 @@ ol.inherits(ol.featureAnimation.Drop, ol.featureAnimation);
 ol.featureAnimation.Drop.prototype.animate = function (e)
 {	// First time > calculate duration / speed
 	if (!e.time) 
-	{	if (this.side_=='top') this.dy = e.extent[3]-e.bbox[1];
-		else this.dy = e.extent[1]-e.bbox[3];
-		if (this.speed_) this.duration_ = Math.abs(this.dy)/this.speed_/e.frameState.viewState.resolution;
+	{	var angle = e.frameState.viewState.rotation;
+		var s = e.frameState.size[1] * e.frameState.viewState.resolution;
+		if (this.side_!='top') s *= -1;
+		this.dx = -Math.sin(angle)*s;
+		this.dy = Math.cos(angle)*s;
+		if (this.speed_) 
+		{	this.duration_ = s/this.speed_/e.frameState.viewState.resolution;
+		}
 	}
 	// Animate
 	var flashGeom = e.geom.clone();
-	flashGeom.translate(0,  this.dy*(1-this.easing_(e.elapsed)));
+	flashGeom.translate(
+		this.dx*(1-this.easing_(e.elapsed)),  
+		this.dy*(1-this.easing_(e.elapsed))
+	);
 	this.drawGeom_(e, flashGeom, e.geom);
 	
 	return (e.time <= this.duration_);
