d9866c4b632606974a4eab3228603ac584727b8e
Jean-Marc Viglino <jean-marc.viglino@ign.fr>
2017-12-19 10:21:51 +0100

    [ADD] doc files

---
+++ doc/namespace.js
@@ -0,0 +1,25 @@
+/** @namespace ol */
+/** @namespace ol.layer */
+/** @namespace ol.layer.Vector */
+/** @namespace ol.source */
+/** @namespace ol.control */
+/** @namespace ol.interaction */
+/** @namespace ol.filter */
+/** @namespace ol.style */
+
+/** The map is the core component of OpenLayers. 
+ * For a map to render, a view, one or more layers, and a target container are needed:
+ * @class ol.Map 
+ * @see (@link http://openlayers.org/en/latest/apidoc/ol.Map.html)
+*/
+/** Openlayers Overlay.    
+ * An element to be displayed over the map and attached to a single map location. 
+ * @class ol.Overlay
+ * @see {@link http://openlayers.org/en/latest/apidoc/ol.Overlay.html}
+ */
+/** Openlayers base class for controls.    
+ * A control is a visible widget with a DOM element in a fixed position on the screen. 
+ * They can involve user input (buttons), or be informational only; the position is determined using CSS. 
+ * @class ol.control.Control
+ * @see {@link http://openlayers.org/en/latest/apidoc/ol.control.Control.html}
+ */
