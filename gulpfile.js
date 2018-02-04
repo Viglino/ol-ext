a6bc02c48b970bea2ef8a875d10fff4b3b6e4e95
Viglino <jean-marc.viglino@ign.fr>
2018-02-04 08:40:32 +0100

    [BUG] Missing sources

--- gulpfile.js
+++ gulpfile.js
@@ -92,6 +92,7 @@ gulp.task("js", function() {
 		"./src/featureanimation/FeatureAnimation.js", "./src/featureanimation/*.js",
 		"./src/filter/Base.js", "./src/filter/Mask.js", "./src/filter/*.js",
 		"./src/interaction/*.js",
+		"./src/source/*.js",
 		"./src/layer/*.js",
 		"./src/overlay/*.js",
     "./src/geom/*.js",
