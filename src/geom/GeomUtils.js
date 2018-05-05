3ab5b42461865b616bdf2002871d3b10e5859554
Viglino <jean-marc.viglino@ign.fr>
2018-05-05 17:46:46 +0200

    [ADD] ol.geom.offsetCoords + hash lines example

--- src/geom/GeomUtils.js
+++ src/geom/GeomUtils.js
@@ -15,18 +15,19 @@ import ol_extent from 'ol/extent'
 * @param {ol.coordinate} p2 second point
 * @return {number} distance
 */
-var ol_coordinate_dist2d = function(p1, p2)
+ol_coordinate.dist2d = function(p1, p2)
 {	var dx = p1[0]-p2[0];
 	var dy = p1[1]-p2[1];
 	return Math.sqrt(dx*dx+dy*dy);
 }
+
 /** 2 points are equal
 *	Usefull geometric functions
 * @param {ol.coordinate} p1 first point
 * @param {ol.coordinate} p2 second point
 * @return {boolean}
 */
-var ol_coordinate_equal = function(p1, p2)
+ol_coordinate.equal = function(p1, p2)
 {	return (p1[0]==p2[0] && p1[1]==p2[1]);
 }
 
@@ -34,7 +35,7 @@ var ol_coordinate_equal = function(p1, p2)
 * @param {ol.Feature} f
 * @return {ol.coordinate} the center
 */
-var ol_coordinate_getFeatureCenter = function(f)
+ol_coordinate.getFeatureCenter = function(f)
 {	return ol_coordinate.getGeomCenter (f.getGeometry());
 };
 
@@ -42,7 +43,7 @@ var ol_coordinate_getFeatureCenter = function(f)
 * @param {ol.Feature} geom
 * @return {ol.coordinate} the center
 */
-var ol_coordinate_getGeomCenter = function(geom)
+ol_coordinate.getGeomCenter = function(geom)
 {	switch (geom.getType())
 	{	case 'Point': 
 			return geom.getCoordinates();
@@ -120,3 +121,70 @@ ol_geom_LineString.prototype.splitAt = function(pt, tol)
 	else return [this];
 }
 
+/** Offset a polyline 
+ * @param {Array<ol.coordinate} coords
+ * @param {Number} offset
+ * @return {Array<ol.coordinates} resulting coord
+ * @see http://stackoverflow.com/a/11970006/796832
+ * @see https://drive.google.com/viewerng/viewer?a=v&pid=sites&srcid=ZGVmYXVsdGRvbWFpbnxqa2dhZGdldHN0b3JlfGd4OjQ4MzI5M2Y0MjNmNzI2MjY
+ */
+ol.coordinate.offsetCoords = function (coords, offset) {
+	var path = [];
+	var N = coords.length-1;
+	var max = N;
+	var mi, mi1, li, li1, ri, ri1, si, si1, Xi1, Yi1;
+	var p0, p1, p2;
+	var isClosed = ol_coordinate.equal(coords[0],coords[N]);
+	if (!isClosed) {
+		p0 = coords[0];
+		p1 = coords[1];
+		p2 = [
+			p0[0] + (p1[1] - p0[1]) / ol_coordinate.dist2d(p0,p1) *offset,
+			p0[1] - (p1[0] - p0[0]) / ol_coordinate.dist2d(p0,p1) *offset
+		];
+		path.push(p2);
+		coords.push(coords[N])
+		N++;
+		max--;
+	}
+	for (var i = 0; i < max; i++) {
+		p0 = coords[i];
+		p1 = coords[(i+1) % N];
+		p2 = coords[(i+2) % N];
+
+		mi = (p1[1] - p0[1])/(p1[0] - p0[0]);
+        mi1 = (p2[1] - p1[1])/(p2[0] - p1[0]);
+        li = Math.sqrt((p1[0] - p0[0])*(p1[0] - p0[0])+(p1[1] - p0[1])*(p1[1] - p0[1]));
+        li1 = Math.sqrt((p2[0] - p1[0])*(p2[0] - p1[0])+(p2[1] - p1[1])*(p2[1] - p1[1]));
+    	ri = p0[0]+offset*(p1[1] - p0[1])/li;
+        ri1 = p1[0]+offset*(p2[1] - p1[1])/li1;
+        si = p0[1]-offset*(p1[0] - p0[0])/li;
+        si1 = p1[1]-offset*(p2[0] - p1[0])/li1;
+        Xi1 = (mi1*ri1-mi*ri+si-si1)/(mi1-mi);
+		Yi1 = (mi*mi1*(ri1-ri)+mi1*si-mi*si1)/(mi1-mi);
+		
+        // Correction for vertical lines
+		if(p1[0] - coords[i % N][0] == 0) {
+            Xi1 = p1[0] + offset*(p1[1] - coords[i % N][1])/Math.abs(p1[1] - coords[i % N][1]);
+            Yi1 = mi1*Xi1 - mi1*ri1 + si1;
+        }
+        if (p2[0] - p1[0] == 0 ) {
+            Xi1 = p2[0] + offset*(p2[1] - p1[1])/Math.abs(p2[1] - p1[1]);
+            Yi1 = mi*Xi1 - mi*ri + si;
+        }
+        
+		path.push([Xi1, Yi1]);
+	}
+	if (!isClosed) {
+		coords.pop();
+		p0 = coords[coords.length-1];
+		p1 = coords[coords.length-2];
+		p2 = [
+			p0[0] - (p1[1] - p0[1]) / ol_coordinate.dist2d(p0,p1) *offset,
+			p0[1] + (p1[0] - p0[0]) / ol_coordinate.dist2d(p0,p1) *offset
+		];
+		path.push(p2);
+
+	}
+	return path;
+}
