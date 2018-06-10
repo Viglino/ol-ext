dcf9b32849b7558c02e0c9506d899260ac7a2ba7
Viglino <jean-marc.viglino@ign.fr>
2018-06-10 08:02:35 +0200

    [ADD] ModifyTouch interaction

---
+++ src/interaction/ModifyTouch.js
@@ -0,0 +1,136 @@
+/*	Copyright (c) 2016 Jean-Marc VIGLINO, 
+	released under the CeCILL-B license (French BSD license)
+	(http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt).
+*/
+
+import ol from 'ol'
+import ol_interaction_Modify from 'ol/interaction/Modify'
+import ol_Overlay_Popup from '../overlay/Popup'
+
+/** Modify interaction with a popup to delet a point on touch device
+ * @constructor
+ * @extends {ol.interaction.Modify}
+ * @param {olx.interaction.ModifyOptions} options
+ */
+var ol_interaction_ModifyTouch = function(options) {
+  var self = this;
+  if (!options) options = {};
+
+  this._popup = new ol_Overlay_Popup ({
+    popupClass: options.calssName || 'modifytouch',
+    popupClass: "tooltips", // "default", "tooltips", "warning" "black" "default", "tips", "shadow",
+    positioning: 'bottom-rigth',
+    offsetBox: 5
+  });
+
+  this._source = options.source;
+  this._features = options.features;
+
+  // popup content
+  var a = document.createElement('a');
+  a.appendChild(document.createTextNode("delete vertex"));
+  a.onclick = function() {
+    self.removePoint();
+  };
+  this.setPopupContent(a);
+
+  var pixelTolerance = options.pixelTolerance || 0;
+  var searchDist = pixelTolerance +5;
+
+  // Check if there is a feature to select
+  options.condition = function(e) {
+		var features = this.getMap().getFeaturesAtPixel(e.pixel,{
+		  hitTolerance: searchDist
+    });
+    var found = false;
+		if (features) {
+      var search = this._features;
+      if (!search) {
+        var p0 = [e.pixel[0] - searchDist, e.pixel[1] - searchDist]
+        var p1 = [e.pixel[0] + searchDist, e.pixel[1] + searchDist]
+        p0 = this.getMap().getCoordinateFromPixel(p0);
+        p1 = this.getMap().getCoordinateFromPixel(p1);
+        var ext = ol.extent.boundingExtent([p0,p1]);
+        search = this._source.getFeaturesInExtent(ext);
+      } 
+      if (search.getArray) search = search.getArray();
+      for (var i=0, f; f=features[i]; i++) {
+        if (search.indexOf(f) >= 0) break;
+      }
+      if (f) {
+        var p0 = e.pixel;
+        var p1 = f.getGeometry().getClosestPoint(e.coordinate);
+        p1 = this.getMap().getPixelFromCoordinate(p1);
+        var dx = p0[0] - p1[0];
+        var dy = p0[1] - p1[1];
+        found = (Math.sqrt(dx*dx+dy*dy) < searchDist);
+      }
+    }
+    // Show popup if any
+    this.showDelete(found ? e : false);
+
+		return true;
+  };
+
+  // Hide popup on insert
+	options.insertVertexCondition = function(e) {
+		this.showDelete(false);
+		return true;
+  }
+
+  ol_interaction_Modify.call(this, options);
+  
+  this.on(['modifystart','modifyend'], function(){
+		this.showDelete({ modifying: true });
+  });
+};
+ol.inherits(ol_interaction_ModifyTouch, ol_interaction_Modify);
+
+/**
+ * Remove the interaction from its current map, if any,  and attach it to a new
+ * map, if any. Pass `null` to just remove the interaction from the current map.
+ * @param {ol.Map} map Map.
+ * @api stable
+ */
+ol_interaction_ModifyTouch.prototype.setMap = function(map) {	
+  if (this.getMap()) {
+    this.getMap().removeOverlay(this._popup);
+  }
+	ol_interaction_Modify.prototype.setMap.call (this, map);
+  if (this.getMap()) {
+    this.getMap().addOverlay(this._popup);
+  }
+};
+
+ol_interaction_ModifyTouch.prototype.removePoint = function(map) {	
+  ol_interaction_Modify.prototype.removePoint.call (this);
+  this.showDelete(false);
+}
+
+/**
+ * Show the delete menu
+ * @param {Event} e
+ * @api stable
+ */
+ol_interaction_ModifyTouch.prototype.showDelete = function(e) {
+  if (e && e.coordinate) this._popup.show(e.coordinate, this._menu);
+  else this._popup.hide();
+};
+
+/**
+ * Change the popup content
+ * @param {DOMElement} html 
+ */
+ol_interaction_ModifyTouch.prototype.setPopupContent = function(html) {
+  this._menu = html;
+}
+
+/**
+ * Get the popup content
+ * @return {DOMElement}
+ */
+ol_interaction_ModifyTouch.prototype.getPopupContent = function() {
+  return this._menu;
+}
+
+export default ol_interaction_ModifyTouch
