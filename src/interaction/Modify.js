567c422016159b16bb43415f5e0f5007f5dbfca3
Viglino <jean-marc.viglino@ign.fr>
2018-11-04 19:11:46 +0100

    [ADD] getModifiedFeatures

---
+++ src/interaction/Modify.js
@@ -0,0 +1,19 @@
+import ol_interaction_Modify from 'ol/interaction/Modify'
+import {getUid as ol_util_getUid} from 'ol/util'
+
+/* Extent the ol/interaction/Modify with a getModifyFeatures
+ * Get the features modified by the interaction
+ * @return {Array<ol.Feature>} the modified features
+ */
+ol_interaction_Modify.prototype.getModifiedFeatures = function() {
+  var featuresById = {};
+  this.dragSegments_.forEach( function(s) {
+    var feature = s[0].feature;
+    featuresById[ol_util_getUid(feature)] = feature;
+  });
+  var features = [];
+  for (var i in featuresById) features.push(featuresById[i]);
+  return features;
+};
+
+export default ol_interaction_Modify
