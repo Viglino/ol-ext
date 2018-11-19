d1725891a475ad13d32aa3d9c59c129c8bafb006
Viglino <jean-marc.viglino@ign.fr>
2018-11-19 13:38:53 +0100

    [BUILD]

--- dist/ol-ext.js
+++ dist/ol-ext.js
@@ -9844,7 +9844,8 @@ ol.interaction.DropFile.prototype.ondrop = function(e)
  * @extends {ol.interaction.Interaction}
  * @fires setattributestart
  * @fires setattributeend
- * @param {*} options ol.interaction.Select options
+ * @param {*} options extentol.interaction.Select options
+ *  @param {boolean} options.cursor use a paint bucket cursor, default true
  * @param {*} properties The properties as key/value
  */
 ol.interaction.FillAttribute = function(options, properties) {
@@ -9856,8 +9857,55 @@ ol.interaction.FillAttribute = function(options, properties) {
     this.getFeatures().clear();
     this.fill(e.selected, this._attributes);
   }.bind(this));
+  if (options.cursor!==false) {
+    var canvas = document.createElement('CANVAS');
+    canvas.width = canvas.height = 32;
+    var ctx = canvas.getContext("2d");
+    ctx.beginPath();
+      ctx.moveTo(9,3);
+      ctx.lineTo(2,9);
+      ctx.lineTo(10,17);
+      ctx.lineTo(17,11);
+    ctx.closePath();
+    ctx.fillStyle = "#fff";
+    ctx.fill();
+    ctx.stroke();
+    ctx.beginPath();
+      ctx.moveTo(6,4);
+      ctx.lineTo(0,8);
+      ctx.lineTo(0,13);
+      ctx.lineTo(3,17);
+      ctx.lineTo(3,8);
+    ctx.closePath();
+    ctx.fillStyle = "#000";
+    ctx.fill();
+    ctx.stroke();
+    ctx.moveTo(8,8);
+    ctx.lineTo(10,0);
+    ctx.lineTo(11,0);
+    ctx.lineTo(13,3);
+    ctx.lineTo(13,7);
+    ctx.stroke();
+    this._cursor = 'url('+canvas.toDataURL()+') 0 13, auto';
+  }
 };
 ol.inherits(ol.interaction.FillAttribute, ol.interaction.Select);
+/** Activate the interaction
+ * @param {boolean} active
+ */
+ol.interaction.FillAttribute.prototype.setActive = function(active) {
+  ol.interaction.Select.prototype.setActive.call(this, active);
+  if (this.getMap() && this._cursor) {
+    if (active) {
+      this._previousCursor = this.getMap().getTargetElement().style.cursor;
+      this.getMap().getTargetElement().style.cursor = this._cursor;
+      console.log('setCursor',this._cursor)
+    } else {
+      this.getMap().getTargetElement().style.cursor = this._previousCursor;
+      this._previousCursor = undefined;
+    }
+  }
+};
 /** Set attributes
  * @param {*} properties The properties as key/value
  */
@@ -13059,7 +13107,13 @@ ol.interaction.Transform.prototype.Cursors = {
  * @api stable
  */
 ol.interaction.Transform.prototype.setMap = function(map) {
-  if (this.getMap()) this.getMap().removeLayer(this.overlayLayer_);
+  if (this.getMap()) {
+    this.getMap().removeLayer(this.overlayLayer_);
+    if (this.previousCursor_) {
+      this.getMap().getTargetElement().style.cursor = this.previousCursor_;
+      this.previousCursor_ = undefined;
+    }
+  }
   ol.interaction.Pointer.prototype.setMap.call (this, map);
   this.overlayLayer_.setMap(map);
   if (map !== null) {
@@ -16251,6 +16305,121 @@ ol.Overlay.PopupFeature.prototype._getHtml = function(feature) {
   this._select.getFeatures().push(feature);
   this._noselect = false;
   return html;
+};
+
+/*	Copyright (c) 2018 Jean-Marc VIGLINO, 
+	released under the CeCILL-B license (French BSD license)
+	(http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt).
+*/
+/** A tooltip element to be displayed over the map and attached on the cursor position.
+ * @constructor
+ * @extends {ol.Overlay.Popup}
+ * @param {} options Extend Popup options 
+ *	@param {String} options.popupClass the a class of the overlay to style the popup.
+ *	@param {Number|Array<number>} options.offsetBox an offset box
+ *	@param {ol.OverlayPositioning | string | undefined} options.positionning 
+ *		the 'auto' positioning var the popup choose its positioning to stay on the map.
+ * @api stable
+ */
+ol.Overlay.Tooltip = function (options) {
+  options = options || {};
+  options.popupClass = options.popupClass || options.className || 'tooltips black';
+  options.positioning = options.positioning || 'center-left';
+	ol.Overlay.Popup.call(this, options);
+  this._interaction = new ol.interaction.Interaction({
+    handleEvent: function(e){
+      if (e.type==='pointermove' || e.type==='click') {
+        if (this.get('info')) {
+          this.show(e.coordinate, this.get('info'));
+        }
+        else this.hide();
+        this._coord = e.coordinate;
+      }
+      return true;
+    }.bind(this)
+  });
+};
+ol.inherits(ol.Overlay.Tooltip, ol.Overlay.Popup);
+/**
+ * Set the map instance the control is associated with
+ * and add its controls associated to this map.
+ * @param {_ol_Map_} map The map instance.
+ */
+ol.Overlay.Tooltip.prototype.setMap = function (map) {
+  if (this.getMap()) this.getMap().removeInteraction(this._interaction);
+  ol.Overlay.Popup.prototype.setMap.call(this, map);
+  if (this.getMap()) this.getMap().addInteraction(this._interaction);
+};
+/** Show the popup. If a feature has been passed to the 
+ * Tooltip the area/length will be added to the Tooltip
+ * @param {ol.Coordinate|string} coordinate the coordinate of the popup or the HTML content.
+ * @param {string|undefined} html the HTML content (undefined = previous content).
+ */
+ol.Overlay.Tooltip.prototype.show = function(coord, html) {
+  // Add measure
+  if (this.get('measure')) html = this.get('measure') +'<br/>'+ html;
+  // Show popup
+  ol.Overlay.Popup.prototype.show.call(this, coord, html);
+};
+/** Set the Tooltip info
+ * If information is not null it will be set with a delay,
+ * thus watever the information is inserted, the significant information will be set.
+ * ie. ttip.setInformation('ok'); ttip.setInformation(null); will set 'ok' 
+ * ttip.set('info,'ok'); ttip.set('info', null); will set null
+ * @param {string} what The information to display in the tooltip, default remove information
+ */
+ol.Overlay.Tooltip.prototype.setInfo = function(what) {
+  if (!what) {
+    this.set('info','');
+    this.hide();
+  }
+  else setTimeout(function() { 
+    this.set('info', what); 
+    this.show(this._coord, this.get('info'));
+  }.bind(this));
+};
+/** Set a feature associated with the tooltips
+ * @param {ol.Feature} feature
+ */
+ol.Overlay.Tooltip.prototype.setFeature = function(feature) {
+  this._feature = feature;
+  if (this._listener) {
+    this._listener.forEach(function(l) {
+      ol.Observable.unByKey(l);
+    });
+  }
+  this._listener = [];
+  this.set('measure', '');
+  if (feature) {
+    this._listener.push(feature.getGeometry().on('change', function(e){ 
+      var geom = e.target;
+      var measure;
+      if (geom.getArea) {
+        var area = ol.sphere.getArea(geom, { projection: this.getMap().getView().getProjection() });
+        area = Math.round(area*100) / 100;
+        if (area) {
+          if (area>10000) {
+            area = (area/1000000).toLocaleString(undefined, {maximumFractionDigits:2}) + ' km²';
+          } else {
+            area = area.toLocaleString(undefined, {maximumFractionDigits:2}) + ' m²';
+          }
+        }
+        measure = area;
+      } else if (geom.getLength) {
+        var length = ol.sphere.getLength(geom, { projection: this.getMap().getView().getProjection() });
+        length = Math.round(length*100) / 100;
+        if (length) {
+          if (length>100) {
+            length = (length/1000).toLocaleString(undefined, {maximumFractionDigits:2}) + ' km';
+          } else {
+            length = length.toLocaleString(undefined, {maximumFractionDigits:2}) + ' m';
+          }
+        }
+        measure = length;
+      }
+      this.set('measure', measure);
+    }.bind(this)));
+  }
 };
 
 /*	Copyright (c) 2018 Jean-Marc VIGLINO, 
