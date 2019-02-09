0034dcb10338050f9e1642c8d897f437d7231fdc
Viglino <jean-marc.viglino@ign.fr>
2019-02-09 18:17:26 +0100

    [ADD] MapZone control

--- src/util/ext.js
+++ src/util/ext.js
@@ -2,3 +2,18 @@
  */
 /*global ol*/
 if (window.ol && !ol.ext)  ol.ext = {};
+
+/** Inherit the prototype methods from one constructor into another.
+ * replace deprecated ol method
+ *
+ * @param {!Function} childCtor Child constructor.
+ * @param {!Function} parentCtor Parent constructor.
+ * @function module:ol.inherits
+ * @api
+ */
+var ol_ext_inherits = function(child,parent) {
+    child.prototype = Object.create(parent.prototype);
+    child.prototype.constructor = child;
+};
+
+export default ol_ext_inherits
