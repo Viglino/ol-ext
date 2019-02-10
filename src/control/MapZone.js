b8329e83228b68a7b75b0b710ede9dd1e19ad1dd
Viglino <jean-marc.viglino@ign.fr>
2019-02-10 11:17:21 +0100

    [UPD] MapZone extent in EPSG:4326

--- src/control/MapZone.js
+++ src/control/MapZone.js
@@ -20,8 +20,8 @@ import ol_ext_element from 'ol-ext/util/element'
  * @param {Object=} options Control options.
  *	@param {string} options.className class name
  *	@param {ol.layer.Layer} options.layer layer to display in the control
- *	@param {ol.ProjectionLike} options.prokjection projection of the control, Default is EPSG:3857 (Spherical Mercator).
- *  @param {Array<any>} options.zone a list of zone: { name, extent }
+ *	@param {ol.ProjectionLike} options.projection projection of the control, Default is EPSG:3857 (Spherical Mercator).
+ *  @param {Array<any>} options.zone an array of zone: { name, extent (in EPSG:4326) }
  *  @param {bolean} options.centerOnClick center on click when click on zones, default true
  */
 var ol_control_MapZone = function(options) {
@@ -62,17 +62,20 @@ var ol_control_MapZone = function(options) {
   // Create maps
   var maps = [];
   options.zones.forEach(function(z) {
+    var view = new ol_View({ zoom: 6, center: [0,0], projection: options.projection });
+    var extent = ol_proj.transformExtent(z.extent, 'EPSG:4326', view.getProjection());
+    console.log(extent, z.extent)
     var div = ol_ext_element.create('DIV', {
       className: 'ol-mapzonezone',
       parent: element,
       click : function() {
         this.dispatchEvent({
           type: 'select',
-          coordinate: ol_extent_getCenter(z.extent),
-          extent: z.extent
+          coordinate: ol_extent_getCenter(extent),
+          extent: extent
         });
         if (options.centerOnClick !== false) {
-          this.getMap().getView().fit(z.extent);
+          this.getMap().getView().fit(extent);
         }
         this.setVisible(false);
       }.bind(this)
@@ -80,7 +83,6 @@ var ol_control_MapZone = function(options) {
     var layer = new options.layer.constructor({
       source: options.layer.getSource()
     });
-    var view = new ol_View({ zoom: 6, center: [0,0], projection: options.projection });
     var map = new ol_Map({
       target: div,
       view: view,
@@ -89,7 +91,7 @@ var ol_control_MapZone = function(options) {
       layers: [layer]
     });
     maps.push(map);
-    view.fit(z.extent);
+    view.fit(extent);
     // Nmae
     ol_ext_element.create('P', {
       html: z.title,
@@ -114,38 +116,49 @@ ol_control_MapZone.prototype.setVisible = function (b) {
   else this.element.classList.add('ol-collapsed');
 };
 
-/** Pre-defined zones
- * - French Terrritory
- */
-ol_control_MapZone.zones = {
-  'DOMTOM': [{
-      title: 'Métropole',
-      extent: [-592044, 5036092, 1083455, 6660226]
-    }, {
-      title: 'Guadeloupe',
-      extent: [-6890520, 1776510, -6785801, 1871445]
-    },{
-      title: 'Martinique',
-      extent: [-6819160, 1618712, -6764813, 1677110]
-    },{
-      title: 'Guyane',
-      extent: [-6091988, 240549, -5736097, 646583]
-    },{
-      title: 'La réunion',
-      extent: [6141562, -2440557, 6220751, -2374669]
-    },{
-      title: 'Mayotte',
-      extent: [5004907, -1461645, 5048705, -1420369]
-    },{
-      title: 'Polynésie Française',
-      extent: [22958158, -2534237, 24697254, -1213405]
-    },{
-      title: 'Nouvelle Calédonie',
-      extent: [18230148, -2581503, 18664922, -2251295]
-    },{
-      title: 'St-Pierre et Miquelon',
-      extent: [-6284397, 5900469, -6244803, 5966052]
-    }]
-};
+/** Pre-defined zones */
+ol_control_MapZone.zones = {};
+  
+/** French overseas departments  */
+ol_control_MapZone.zones.DOM = [{
+  "title": "Guadeloupe",
+  "extent": [ -61.898594315312444, 15.75623038647845, -60.957887532935324, 16.575317670979473 ]
+},{
+  "title": "Guyane",
+  "extent": [ -54.72525931072715, 2.1603763430019, -51.528236062921344, 5.7984307809552575 ]
+},{
+  "title": "Martinique",
+  "extent": [ -61.257556528564756, 14.387506317407514, -60.76934912110432, 14.895067461729951 ]
+},{
+  "title": "Mayotte",
+  "extent": [ 44.959844536967815, -13.01674138212816, 45.35328866510648, -12.65521942207829 ]
+},{
+  "title": "La réunion",
+  "extent": [ 55.17059012967656, -21.407680069231688, 55.88195702001797, -20.85560221637526 ]
+}];
+
+/** French overseas territories */
+ol_control_MapZone.zones.TOM = [{
+  "title": "Polynésie Française",
+  "extent": [ 206.23664226630862, -22.189040615809787, 221.85920743981987, -10.835039595040698 ]
+},{
+  "title": "Nouvelle Calédonie",
+  "extent": [ 163.76420580160925, -22.581641092751838, 167.66984709498706, -19.816411635668445 ]
+},{
+  "title": "St-Pierre et Miquelon",
+  "extent": [ -56.453698765748676, 46.74449858188555, -56.0980198121544, 47.14669874229787 ]
+},{
+  "title": "Wallis et Futuna",
+  "extent": [ 181.7588623143665, -14.7341169873267, 183.95612353301715, -13.134720799175085 ]
+},{
+  "title": "St-Martin St-Barthélemy",
+  "extent": [ -63.1726389501678, 17.806097291313506, -62.7606535945649, 18.13267688837938 ]
+}];
+
+/** French overseas departments and territories */
+ol_control_MapZone.zones.DOMTOM = [{
+  title: 'Métropole',
+  extent: [ -5.318421740712579, 41.16082274292913, 9.73284186155716, 51.21957336557702 ]
+}].concat(ol_control_MapZone.zones.DOM,ol_control_MapZone.zones.TOM);
 
 export default ol_control_MapZone
