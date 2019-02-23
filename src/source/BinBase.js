b95c2466bbb6f7cbd57c05454c1629e24dfb629e
Viglino <jean-marc.viglino@ign.fr>
2019-02-23 17:01:17 +0100

    [UPD] enable source clear(true) for performance enhancement with skip dispatching of removefeature events.

--- src/source/BinBase.js
+++ src/source/BinBase.js
@@ -5,7 +5,6 @@
 import ol_Feature from 'ol/Feature'
 import ol_geom_Polygon from 'ol/geom/Polygon'
 import ol_source_Vector from 'ol/source/Vector'
-import { getCenter as ol_extent_getCenter } from 'ol/extent'
 import { ol_coordinate_getFeatureCenter } from "../geom/GeomUtils";
 
 import {ol_ext_inherits} from '../util/ext'
@@ -21,6 +20,7 @@ import {ol_ext_inherits} from '../util/ext'
 var ol_source_BinBase = function (options) {
   options = options || {};
   this._bindModify = this._onModifyFeature.bind(this);
+  this._watch === true;
 
   ol_source_Vector.call(this, options);
 
@@ -35,6 +35,8 @@ var ol_source_BinBase = function (options) {
   // Future features
   this._origin.on("addfeature", this._onAddFeature.bind(this));
   this._origin.on("removefeature", this._onRemoveFeature.bind(this));
+  this._origin.on("clearstart", this._onClearFeature.bind(this));
+  this._origin.on("clearend", this._onClearFeature.bind(this));
   if (typeof (options.flatAttributes) === 'function') this._flatAttributes = options.flatAttributes;
 };
 ol_ext_inherits(ol_source_BinBase, ol_source_Vector);
@@ -45,11 +47,11 @@ ol_ext_inherits(ol_source_BinBase, ol_source_Vector);
  * @param {ol.Feature} bin
  * @private
  */
-ol_source_BinBase.prototype._onAddFeature = function (e, bin) {
+ol_source_BinBase.prototype._onAddFeature = function (e, bin, listen) {
   var f = e.feature || e.target;
   bin = bin || this.getBinAt(this._geomFn(f), true);
-  bin.get('features').push(f);
-  f.on("change", this._bindModify);
+  if (bin) bin.get('features').push(f);
+  if (listen!==false) f.on("change", this._bindModify);
 };
 
 /**
@@ -58,7 +60,8 @@ ol_source_BinBase.prototype._onAddFeature = function (e, bin) {
  *  @param {ol.Feature} bin
  *  @private
  */
-ol_source_BinBase.prototype._onRemoveFeature = function (e, bin) {
+ol_source_BinBase.prototype._onRemoveFeature = function (e, bin, listen) {
+  if (!this._watch) return;
   var f = e.feature || e.target;
   bin = bin || this.getBinAt(this._geomFn(f));
   if (bin) {
@@ -75,9 +78,24 @@ ol_source_BinBase.prototype._onRemoveFeature = function (e, bin) {
       this.removeFeature(bin);
     }
   } else {
-    console.log("[ERROR:Bin] remove feature: feature doesn't exists anymore.");
+    // console.log("[ERROR:Bin] remove feature: feature doesn't exists anymore.");
   }
-  f.un("change", this._bindModify);
+  if (listen!==false) f.un("change", this._bindModify);
+};
+
+/** When clearing features remove the listener
+ * @private
+ */
+ol_source_BinBase.prototype._onClearFeature = function (e) {
+  if (e.type==='clearstart') {
+    this._origin.getFeatures().forEach(function (f) {
+      f.un("change", this._bindModify);
+    });
+    this.clear();
+    this._watch = false;
+  } else {
+    this._watch = true;
+  }
 };
 
 /**
@@ -98,10 +116,11 @@ ol_source_BinBase.prototype.getBin = function (feature) {
 
 /** Get the grid geometry at the coord 
  * @param {ol.Coordinate} coord
+ * @param {Object} attributes add key/value to this object to add properties to the grid feature
  * @returns {ol.geom.Polygon} 
  * @api
  */
-ol_source_BinBase.prototype.getGridGeomAt = function (coord) {
+ol_source_BinBase.prototype.getGridGeomAt = function (coord /*, attributes */) {
   return new ol_geom_Polygon([coord]);
 };
 
@@ -111,12 +130,17 @@ ol_source_BinBase.prototype.getGridGeomAt = function (coord) {
  * @return {ol.Feature} the bin or null it doesn't exit
  */
 ol_source_BinBase.prototype.getBinAt = function (coord, create) {
-  var g = this.getGridGeomAt(coord);
-  var center = ol_extent_getCenter(g.getExtent());
+  var attributes = {};
+  var g = this.getGridGeomAt(coord, attributes);
+  if (!g) return null;
+  var center = g.getInteriorPoint ? g.getInteriorPoint().getCoordinates() : g.getInteriorPoints().getCoordinates()[0];// ol_extent_getCenter(g.getExtent());
   var features = this.getFeaturesAtCoordinate( center );
   var bin = features[0];
   if (!bin && create) {
-    bin = new ol_Feature({ geometry: g, features: [], center: center });
+    attributes.geometry = g;
+    attributes.features = [];
+    attributes.center = center;
+    bin = new ol_Feature(attributes);
     this.addFeature(bin);
   }
   return bin || null;
@@ -130,11 +154,15 @@ ol_source_BinBase.prototype.getBinAt = function (coord, create) {
 ol_source_BinBase.prototype._onModifyFeature = function (e) {
   var bin = this.getBin(e.target);
   var bin2 = this.getBinAt(this._geomFn(e.target), 'create');
-  if (bin && bin !== bin2) {
+  if (bin !== bin2) {
     // remove from the bin
-    this._onRemoveFeature(e, bin);
+    if (bin) {
+      this._onRemoveFeature(e, bin, false);
+    }
     // insert in the new bin
-    this._onAddFeature(e, bin2);
+    if (bin2) {
+      this._onAddFeature(e, bin2, false);
+    }
   }
   this.changed();
 };
