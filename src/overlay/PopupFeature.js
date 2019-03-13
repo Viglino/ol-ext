097971ea14d8593963b441d8a7128ab14e990230
Viglino <jean-marc.viglino@ign.fr>
2019-03-13 13:57:10 +0100

    [ADD] new options. Template is no more required

--- src/overlay/PopupFeature.js
+++ src/overlay/PopupFeature.js
@@ -1,3 +1,4 @@
+
 /*	Copyright (c) 2018 Jean-Marc VIGLINO, 
   released under the CeCILL-B license (French BSD license)
   (http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt).
@@ -7,22 +8,24 @@ import ol_Overlay_Popup from './Popup'
 import ol_ext_element from '../util/element'
 
 /**
-* A popup element to be displayed on a feature.
-*
-* @constructor
-* @extends {ol_Overlay_Popup}
-* @param {} options Extend Popup options 
-*	@param {String} options.popupClass the a class of the overlay to style the popup.
-*	@param {bool} options.closeBox popup has a close box, default false.
-*	@param {function|undefined} options.onclose: callback function when popup is closed
-*	@param {function|undefined} options.onshow callback function when popup is shown
-*	@param {Number|Array<number>} options.offsetBox an offset box
-*	@param {ol.OverlayPositioning | string | undefined} options.positionning 
-*		the 'auto' positioning var the popup choose its positioning to stay on the map.
-* @param {*} options.template A template with a list of properties to use in the popup
-* @param boolean} options.canFix Enable popup to be fixed 
-* @api stable
-*/
+ * A popup element to be displayed on a feature.
+ *
+ * @constructor
+ * @extends {ol_Overlay_Popup}
+ * @param {} options Extend Popup options 
+ *  @param {String} options.popupClass the a class of the overlay to style the popup.
+ *  @param {bool} options.closeBox popup has a close box, default false.
+ *  @param {function|undefined} options.onclose: callback function when popup is closed
+ *  @param {function|undefined} options.onshow callback function when popup is shown
+ *  @param {Number|Array<number>} options.offsetBox an offset box
+ *  @param {ol.OverlayPositioning | string | undefined} options.positionning 
+ *    the 'auto' positioning var the popup choose its positioning to stay on the map.
+ *  @param {*} options.template A template with a list of properties to use in the popup
+ *  @param {boolean} options.canFix Enable popup to be fixed, default false
+ *  @param {boolean} options.showImage display image url as image, default false
+ *  @param {boolean} options.maxChar max char to display in a cell, default 200
+ *  @api stable
+ */
 var ol_Overlay_PopupFeature = function (options) {
   options = options || {};
 
@@ -30,6 +33,8 @@ var ol_Overlay_PopupFeature = function (options) {
 
   this.setTemplate(options.template);
   this.set('canFix', options.canFix)
+  this.set('showImage', options.showImage)
+  this.set('maxChar', options.maxChar||200)
 
   // Bind with a select interaction
   if (options.select && (typeof options.select.on ==='function')) {
@@ -45,8 +50,8 @@ ol_inherits(ol_Overlay_PopupFeature, ol_Overlay_Popup);
  * @param {*} template A template with a list of properties to use in the popup
  */
 ol_Overlay_PopupFeature.prototype.setTemplate = function(template) {
-  this._template = template || {};
-  if (this._template.attributes instanceof Array) {
+  this._template = template;
+  if (this._template && this._template.attributes instanceof Array) {
     var att = {};
     this._template.attributes.forEach(function (a) {
       att[a] = true;
@@ -69,7 +74,7 @@ ol_Overlay_PopupFeature.prototype.show = function(coordinate, features) {
   var html = this._getHtml(features[0]);
   this.hide();
   if (html) {
-    if (!coordinate) {
+    if (!coordinate || features[0].getGeometry().getType()==='Point') {
       coordinate = features[0].getGeometry().getFirstCoordinate();
     }
     ol_Overlay_Popup.prototype.show.call(this, coordinate, html);
@@ -88,25 +93,47 @@ ol_Overlay_PopupFeature.prototype._getHtml = function(feature) {
         this.element.classList.toggle('ol-fixed');
       }.bind(this));
   }
-  if (this._template.title) {
+  var template = this._template;
+  // calculate template
+  if (!template || !template.attributes) {
+    template = template || {};
+    template. attributes = {};
+    for (var i in feature.getProperties()) if (i!='geometry') {
+      template.attributes[i] = i;
+    }
+  }
+  // Display title
+  if (template.title) {
     var title;
-    if (typeof this._template.title === 'function') {
-      title = this._template.title(feature);
+    if (typeof template.title === 'function') {
+      title = template.title(feature);
     } else {
-      title = feature.get(this._template.title);
+      title = feature.get(template.title);
     }
     ol_ext_element.create('H1', { html:title, parent: html });
   }
-  if (this._template.attributes) {
+  // Display properties in a table
+  if (template.attributes) {
     var tr, table = ol_ext_element.create('TABLE', { parent: html });
-    var atts = this._template.attributes;
+    var atts = template.attributes;
     for (var att in atts) {
       var a = atts[att];
       tr = ol_ext_element.create('TR', { parent: table });
       ol_ext_element.create('TD', { html: a.title || att, parent: tr });
-      var val = feature.get(att);
+      var content, val = feature.get(att);
+      // Show image or content
+      if (this.get('showImage') && /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/.test(val)) {
+        content = ol_ext_element.create('IMG',{
+          src: val
+        });
+      } else {
+        content = (a.before||'') + (a.format ? a.format(val) : val) + (a.after||'');
+        var maxc = this.get('maxChar') || 200;
+        if (typeof(content) === 'string' && content.length>maxc) content = content.substr(0,maxc)+'[...]';
+      }
+      // Add value
       ol_ext_element.create('TD', { 
-        html: (a.before||'') + (a.format ? a.format(val) : val) + (a.after||''), 
+        html: content, 
         parent: tr 
       });
     }
@@ -114,7 +141,15 @@ ol_Overlay_PopupFeature.prototype._getHtml = function(feature) {
   // Zoom button
   ol_ext_element.create('BUTTON', { className: 'ol-zoombt', parent: html })
     .addEventListener('click', function() {
-      this.getMap().getView().fit(feature.getGeometry().getExtent(), { duration:1000 });
+      if (feature.getGeometry().getType()==='Point') {
+        this.getMap().getView().animate({
+          center: feature.getGeometry().getFirstCoordinate(),
+          zoom:  Math.max(map.getView().getZoom(), 18)
+        });
+      } else  {
+        var ext = feature.getGeometry().getExtent();
+        this.getMap().getView().fit(ext, { duration:1000 });
+      }
     }.bind(this));
 
   // Counter
