5c06f7b0b060f597da91cbad5a84a9484d7e701a
Viglino <jean-marc.viglino@ign.fr>
2019-08-16 12:15:43 +0200

    [ADD] reverse geocoding

--- src/control/SearchNominatim.js
+++ src/control/SearchNominatim.js
@@ -1,6 +1,6 @@
 /*	Copyright (c) 2017 Jean-Marc VIGLINO,
-	released under the CeCILL-B license (French BSD license)
-	(http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt).
+  released under the CeCILL-B license (French BSD license)
+  (http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt).
 */
 import ol_ext_inherits from '../util/ext'
 import {transform as ol_proj_transform} from 'ol/proj'
@@ -13,40 +13,43 @@ import ol_control_SearchJSON from "./SearchJSON";
  * @extends {ol.control.Search}
  * @fires select
  * @param {Object=} Control options.
- *	@param {string} options.className control class name
- *	@param {boolean | undefined} options.polygon To get output geometry of results (in geojson format), default false.
- *	@param {viewbox | undefined} options.viewbox The preferred area to find search results. Any two corner points of the box are accepted in any order as long as they span a real box, default none.
- *	@param {Element | string | undefined} options.target Specify a target if you want the control to be rendered outside of the map's viewport.
- *	@param {string | undefined} options.label Text label to use for the search button, default "search"
- *	@param {string | undefined} options.placeholder placeholder, default "Search..."
- *	@param {number | undefined} options.typing a delay on each typing to start searching (ms), default 500.
- *	@param {integer | undefined} options.minLength minimum length to start searching, default 3
- *	@param {integer | undefined} options.maxItems maximum number of items to display in the autocomplete list, default 10
+ *  @param {string} options.className control class name
+ *  @param {boolean | undefined} options.polygon To get output geometry of results (in geojson format), default false.
+ *  @param {viewbox | undefined} options.viewbox The preferred area to find search results. Any two corner points of the box are accepted in any order as long as they span a real box, default none.
+ *  @param {Element | string | undefined} options.target Specify a target if you want the control to be rendered outside of the map's viewport.
+ *  @param {string | undefined} options.label Text label to use for the search button, default "search"
+ *  @param {string | undefined} options.placeholder placeholder, default "Search..."
+ *  @param {number | undefined} options.typing a delay on each typing to start searching (ms), default 500.
+ *  @param {integer | undefined} options.minLength minimum length to start searching, default 3
+ *  @param {integer | undefined} options.maxItems maximum number of items to display in the autocomplete list, default 10
  *
- *	@param {string|undefined} options.url URL to Nominatim API, default "https://nominatim.openstreetmap.org/search"
+ *  @param {string|undefined} options.url URL to Nominatim API, default "https://nominatim.openstreetmap.org/search"
  * @see {@link https://wiki.openstreetmap.org/wiki/Nominatim}
  */
-var ol_control_SearchNominatim = function(options)
-{	options = options || {};
-    options.className = options.className || 'nominatim';
-    options.typing = options.typing || 500;
-    options.url = options.url || 'https://nominatim.openstreetmap.org/search';
-    options.copy = '<a href="http://www.openstreetmap.org/copyright" target="new">&copy; OpenStreetMap contributors</a>';
-    ol_control_SearchJSON.call(this, options);
-    this.set('polygon', options.polygon);
-    this.set('viewbox', options.viewbox);
+var ol_control_SearchNominatim = function(options) {
+  options = options || {};
+  options.className = options.className || 'nominatim';
+  options.typing = options.typing || 500;
+  options.url = options.url || 'https://nominatim.openstreetmap.org/search';
+  options.copy = '<a href="http://www.openstreetmap.org/copyright" target="new">&copy; OpenStreetMap contributors</a>';
+  ol_control_SearchJSON.call(this, options);
+  this.set('polygon', options.polygon);
+  this.set('viewbox', options.viewbox);
 };
 ol_ext_inherits(ol_control_SearchNominatim, ol_control_SearchJSON);
 
 /** Returns the text to be displayed in the menu
  *	@param {ol.Feature} f the feature
- *	@return {string} the text to be displayed in the index
- *	@api
- */
+*	@return {string} the text to be displayed in the index
+*	@api
+*/
 ol_control_SearchNominatim.prototype.getTitle = function (f) {
-    var title = f.display_name+"<i>"+f.class+" - "+f.type+"</i>";
-    if (f.icon) title = "<img src='"+f.icon+"' />" + title;
-    return (title);
+  var info = [];
+  if (f.class) info.push(f.class);
+  if (f.type) info.push(f.type);
+  var title = f.display_name+(info.length ? "<i>"+info.join(' - ')+"</i>" : '');
+  if (f.icon) title = "<img src='"+f.icon+"' />" + title;
+  return (title);
 };
 
 /** 
@@ -55,28 +58,47 @@ ol_control_SearchNominatim.prototype.getTitle = function (f) {
  * @api
  */
 ol_control_SearchNominatim.prototype.requestData = function (s) {
-	var data = { 
-        format: "json", 
-        addressdetails: 1, 
-        q: s, 
-        polygon_geojson: this.get('polygon') ? 1:0,
-        limit: this.get('maxItems')
-    };
-    if (this.get('viewbox')) data.viewbox = this.get('viewbox');
-    return data;
+  var data = { 
+    format: "json", 
+    addressdetails: 1, 
+    q: s, 
+    polygon_geojson: this.get('polygon') ? 1:0,
+    limit: this.get('maxItems')
+  };
+  if (this.get('viewbox')) data.viewbox = this.get('viewbox');
+  return data;
 };
 
 /** A ligne has been clicked in the menu > dispatch event
  *	@param {any} f the feature, as passed in the autocomplete
- *	@api
- */
+*	@api
+*/
 ol_control_SearchNominatim.prototype.select = function (f){
-    var c = [Number(f.lon), Number(f.lat)];
-    // Add coordinate to the event
-    try {
-        c = ol_proj_transform (c, 'EPSG:4326', this.getMap().getView().getProjection());
-    } catch(e) { /* ok */}
-    this.dispatchEvent({ type:"select", search:f, coordinate: c });
+  var c = [Number(f.lon), Number(f.lat)];
+  // Add coordinate to the event
+  try {
+    c = ol_proj_transform (c, 'EPSG:4326', this.getMap().getView().getProjection());
+  } catch(e) { /* ok */}
+  this.dispatchEvent({ type:"select", search:f, coordinate: c });
+};
+
+/** Reverse geocode
+ * @param {ol.coordinate} coord
+ * @api
+ */
+ol_control_SearchNominatim.prototype.reverseGeocode = function (coord, cback) {
+  var lonlat = ol_proj_transform (coord, this.getMap().getView().getProjection(), 'EPSG:4326');
+  this.ajax(
+    this.get('url').replace('search', 'reverse'),
+    { lon: lonlat[0], lat: lonlat[1], format: 'json' },
+    function(resp) {
+      if (cback) cback.call(this, [resp]);
+      else {
+        this._handleSelect(resp);
+        search.setInput('', true);
+      }
+    }
+  );
 };
 
 export default ol_control_SearchNominatim
--- src/control/SearchPhoton.js
+++ src/control/SearchPhoton.js
@@ -1,6 +1,6 @@
 ﻿/*	Copyright (c) 2017 Jean-Marc VIGLINO,
-	released under the CeCILL-B license (French BSD license)
-	(http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt).
+  released under the CeCILL-B license (French BSD license)
+  (http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt).
 */
 import ol_ext_inherits from '../util/ext'
 import {transform as ol_proj_transform} from 'ol/proj'
@@ -14,28 +14,28 @@ import ol_geom_Point from 'ol/geom/Point'
  * @extends {ol_control_SearchJSON}
  * @fires select
  * @param {Object=} Control options.
- *	@param {string} options.className control class name
- *	@param {Element | string | undefined} options.target Specify a target if you want the control to be rendered outside of the map's viewport.
- *	@param {string | undefined} options.label Text label to use for the search button, default "search"
- *	@param {string | undefined} options.placeholder placeholder, default "Search..."
- *	@param {number | undefined} options.typing a delay on each typing to start searching (ms), default 1000.
- *	@param {integer | undefined} options.minLength minimum length to start searching, default 3
- *	@param {integer | undefined} options.maxItems maximum number of items to display in the autocomplete list, default 10
+ *  @param {string} options.className control class name
+ *  @param {Element | string | undefined} options.target Specify a target if you want the control to be rendered outside of the map's viewport.
+ *  @param {string | undefined} options.label Text label to use for the search button, default "search"
+ *  @param {string | undefined} options.placeholder placeholder, default "Search..."
+ *  @param {number | undefined} options.typing a delay on each typing to start searching (ms), default 1000.
+ *  @param {integer | undefined} options.minLength minimum length to start searching, default 3
+ *  @param {integer | undefined} options.maxItems maximum number of items to display in the autocomplete list, default 10
  *  @param {function | undefined} options.handleResponse Handle server response to pass the features array to the list
  * 
- *	@param {string|undefined} options.url Url to photon api, default "http://photon.komoot.de/api/"
- *	@param {string|undefined} options.lang Force preferred language, default none
- *	@param {boolean} options.position Search, with priority to geo position, default false
- *	@param {function} options.getTitle a function that takes a feature and return the name to display in the index, default return street + name + contry
+ *  @param {string|undefined} options.url Url to photon api, default "http://photon.komoot.de/api/"
+ *  @param {string|undefined} options.lang Force preferred language, default none
+ *  @param {boolean} options.position Search, with priority to geo position, default false
+ *  @param {function} options.getTitle a function that takes a feature and return the name to display in the index, default return street + name + contry
  */
-var ol_control_SearchPhoton = function(options)
-{	options = options || {};
-	options.className = options.className || 'photon';
-	options.url = options.url || 'http://photon.komoot.de/api/';
-	options.copy = '<a href="http://www.openstreetmap.org/copyright" target="new">&copy; OpenStreetMap contributors</a>';
-	ol_control_SearchJSON.call(this, options);
-	this.set('lang', options.lang);
-	this.set('position', options.position);
+var ol_control_SearchPhoton = function(options) {
+  options = options || {};
+  options.className = options.className || 'photon';
+  options.url = options.url || 'http://photon.komoot.de/api/';
+  options.copy = '<a href="http://www.openstreetmap.org/copyright" target="new">&copy; OpenStreetMap contributors</a>';
+  ol_control_SearchJSON.call(this, options);
+  this.set('lang', options.lang);
+  this.set('position', options.position);
 };
 ol_ext_inherits(ol_control_SearchPhoton, ol_control_SearchJSON);
 
@@ -44,15 +44,15 @@ ol_ext_inherits(ol_control_SearchPhoton, ol_control_SearchJSON);
 *	@return {string} the text to be displayed in the index
 *	@api
 */
-ol_control_SearchPhoton.prototype.getTitle = function (f)
-{	var p = f.properties;
-	return (p.housenumber||"")
-		+ " "+(p.street || p.name || "")
-		+ "<i>"
-		+ " "+(p.postcode||"")
-		+ " "+(p.city||"")
-		+ " ("+p.country
-		+ ")</i>";
+ol_control_SearchPhoton.prototype.getTitle = function (f) {
+  var p = f.properties;
+  return (p.housenumber||"")
+    + " "+(p.street || p.name || "")
+    + "<i>"
+    + " "+(p.postcode||"")
+    + " "+(p.city||"")
+    + " ("+p.country
+    + ")</i>";
 };
 
 /** 
@@ -60,22 +60,22 @@ ol_control_SearchPhoton.prototype.getTitle = function (f)
  * @return {Object} request data (as key:value)
  * @api
  */
-ol_control_SearchPhoton.prototype.requestData = function (s)
-{	var data =
-	{	q: s,
-		lang: this.get('lang'),
-		limit: this.get('maxItems')
-	}
-	// Handle position proirity
-	if (this.get('position'))
-	{	var view = this.getMap().getView();
-		var pt = new ol_geom_Point(view.getCenter());
-		pt = (pt.transform (view.getProjection(), "EPSG:4326")).getCoordinates();
+ol_control_SearchPhoton.prototype.requestData = function (s) {
+  var data = {
+    q: s,
+    lang: this.get('lang'),
+    limit: this.get('maxItems')
+  }
+  // Handle position proirity
+  if (this.get('position')) {
+    var view = this.getMap().getView();
+    var pt = new ol_geom_Point(view.getCenter());
+    pt = (pt.transform (view.getProjection(), "EPSG:4326")).getCoordinates();
 
-		data.lon = pt[0];
-		data.lat = pt[1];
-	}
-	return data;
+    data.lon = pt[0];
+    data.lat = pt[1];
+  }
+  return data;
 };
 
 /**
@@ -84,7 +84,7 @@ ol_control_SearchPhoton.prototype.requestData = function (s)
  * @return {Array<any>} an array of feature
  */
 ol_control_SearchPhoton.prototype.handleResponse = function (response) {
-	return response.features;
+  return response.features;
 };
 
 /** Prevent same feature to be drawn twice: test equality
@@ -94,23 +94,50 @@ ol_control_SearchPhoton.prototype.handleResponse = function (response) {
  * @api
  */
 ol_control_SearchPhoton.prototype.equalFeatures = function (f1, f2) {
-	return (this.getTitle(f1) === this.getTitle(f2)
-		&& f1.geometry.coordinates[0] === f2.geometry.coordinates[0]
-		&& f1.geometry.coordinates[1] === f2.geometry.coordinates[1]);
+  return (this.getTitle(f1) === this.getTitle(f2)
+    && f1.geometry.coordinates[0] === f2.geometry.coordinates[0]
+    && f1.geometry.coordinates[1] === f2.geometry.coordinates[1]);
 };
 
 /** A ligne has been clicked in the menu > dispatch event
 *	@param {any} f the feature, as passed in the autocomplete
 *	@api
 */
-ol_control_SearchPhoton.prototype.select = function (f)
-{	var c = f.geometry.coordinates;
-	// Add coordinate to the event
-	try {
-		c = ol_proj_transform (f.geometry.coordinates, 'EPSG:4326', this.getMap().getView().getProjection());
-	} catch(e) { /* ok */ }
-	this.dispatchEvent({ type:"select", search:f, coordinate: c });
+ol_control_SearchPhoton.prototype.select = function (f) {
+  var c = f.geometry.coordinates;
+  // Add coordinate to the event
+  try {
+    c = ol_proj_transform (f.geometry.coordinates, 'EPSG:4326', this.getMap().getView().getProjection());
+  } catch(e) { /* ok */ }
+  this.dispatchEvent({ type:"select", search:f, coordinate: c });
 };
 
-/** */
+/** Get data for reverse geocode 
+ * @param {ol.coordinate} coord
+ */
+ol_control_SearchPhoton.prototype.reverseData = function (coord) {
+  var lonlat = ol_proj_transform (coord, this.getMap().getView().getProjection(), 'EPSG:4326');
+  return { lon: lonlat[0], lat: lonlat[1] };
+};
+
+/** Reverse geocode
+ * @param {ol.coordinate} coord
+ * @api
+ */
+ol_control_SearchPhoton.prototype.reverseGeocode = function (coord, cback) {
+  this.ajax(
+    this.get('url').replace('/api/', '/reverse/').replace('/search/', '/reverse/'),
+    this.reverseData(coord),
+    function(resp) {
+      if (resp.features) resp = resp.features;
+      if (!(resp instanceof Array)) resp = [resp]
+      if (cback) cback.call(this, resp);
+      else {
+        this._handleSelect(resp[0]);
+        search.setInput('', true);
+      }
+    }
+  );
+};
+  
 export default ol_control_SearchPhoton
