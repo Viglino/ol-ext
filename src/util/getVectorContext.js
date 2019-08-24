a1fc27404173eec66ba4b55a3b638a76177be9fc
Viglino <jean-marc.viglino@ign.fr>
2019-08-24 09:56:36 +0200

    [BUG] import getVectorContext error when build by rollup #332 Rewrite the whole function for ol5/6 compatibility.

--- src/util/getVectorContext.js
+++ src/util/getVectorContext.js
@@ -1,21 +1,17 @@
-/* Export getVector context for backward compatibility ol5 / ol6
+/** Export getVector context for backward compatibility ol5 / ol6
  * Create a brand new function for ol5 copy of ol6 function.
+ * Will be ignored using openlayers-ext package or ol5
  */
-import { multiply as multiplyTransform } from 'ol/transform.js';
-import CanvasImmediateRenderer from 'ol/render/canvas/Immediate.js';
+import { multiply as multiplyTransform } from 'ol/transform';
+import CanvasImmediateRenderer from 'ol/render/canvas/Immediate';
 
-/**
- * Gets a vector context for drawing to the event's canvas.
- * @param {import("./render/Event.js").default} event Render event.
- * @returns {CanvasImmediateRenderer} Vector context.
- * @api
- */
 function getVectorContext(event) {
   const frameState = event.frameState;
   const transform = multiplyTransform(event.inversePixelTransform.slice(), frameState.coordinateToPixelTransform);
   return new CanvasImmediateRenderer(
     event.context, frameState.pixelRatio, frameState.extent, transform,
-    frameState.viewState.rotation);
+    frameState.viewState.rotation
+  );
 }
 
 export default getVectorContext
