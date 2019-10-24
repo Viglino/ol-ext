658a188d56e99e25c0834087400ea68932e25af2
Viglino <jean-marc.viglino@ign.fr>
2019-10-24 09:33:22 +0200

    [BUG] getUid with ol6

--- src/interaction/Modify.js
+++ src/interaction/Modify.js
@@ -1,5 +1,6 @@
 import ol_interaction_Modify from 'ol/interaction/Modify'
 import {getUid as ol_getUid} from 'ol/util'
+import {getUid as ol_util_getUid} from 'ol/util'
 
 /* Extent the ol/interaction/Modify with a getModifyFeatures
  * Get the features modified by the interaction
@@ -9,7 +10,10 @@ ol_interaction_Modify.prototype.getModifiedFeatures = function() {
   var featuresById = {};
   this.dragSegments_.forEach( function(s) {
     var feature = s[0].feature;
-    featuresById[ol_getUid(feature)] = feature;
+    // Openlayers > v.6
+    if (window.ol && window.ol.util) featuresById[ol_util_getUid(feature)] = feature;
+    // old version of Openlayers (< v.6) or ol all versions
+    else featuresById[ol_getUid(feature)] = feature;
   });
   var features = [];
   for (var i in featuresById) features.push(featuresById[i]);
