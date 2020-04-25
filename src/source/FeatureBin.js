9406e66a4ddf707f70d47d0df9711c296c57e75c
Viglino <jean-marc.viglino@ign.fr>
2020-04-25 14:15:17 +0200

    [ADD] link a bin source collector to a FeatureBin

--- src/source/FeatureBin.js
+++ src/source/FeatureBin.js
@@ -1,4 +1,4 @@
-  /*	Copyright (c) 2019 Jean-Marc VIGLINO,
+/*	Copyright (c) 2019 Jean-Marc VIGLINO,
   released under the CeCILL-B license (French BSD license)
   (http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt).
 */
@@ -6,11 +6,14 @@ import ol_source_Vector from 'ol/source/Vector'
 import ol_source_BinBase from './BinBase'
 import {ol_ext_inherits} from '../util/ext'
 
-/** A source for INSEE grid
+/** A source that use a set of feature to collect data on it.
+ * If a binSource is provided the bin is recalculated when features change.
  * @constructor
  * @extends {ol.source.Vector}
  * @param {Object} options ol_source_VectorOptions + grid option
- *  @param {ol.source.Vector} options.source Source
+ *  @param {ol.source.Vector} options.source source to collect in the bin
+ *  @param {ol.source.Vector} options.binSource a source to use as bin collector, default none
+ *  @param {Array<ol.Feature>} options.features the features, ignored if binSource is provided, default none
  *  @param {number} [options.size] size of the grid in meter, default 200m
  *  @param {function} [options.geometryFunction] Function that takes an ol.Feature as argument and returns an ol.geom.Point as feature's center.
  *  @param {function} [options.flatAttributes] Function takes a bin and the features it contains and aggragate the features in the bin attributes when saving
@@ -18,13 +21,28 @@ import {ol_ext_inherits} from '../util/ext'
 var ol_source_FeatureBin = function (options) {
   options = options || {};
 
-  this._sourceFeature = new ol_source_Vector ({ features: options.features || [] });
+  if (options.binSource) {
+    this._sourceFeature = options.binSource;
+    // When features change recalculate the bin...
+    var timout;
+    this._sourceFeature.on(['addfeature','changefeature','removefeature'], function(e) {
+      if (timout) {
+        // Do it only one time
+        clearTimeout(timout);
+      }
+      timout = setTimeout(function () {
+        this.reset();
+      }.bind(this));
+    }.bind(this));
+  } else {
+    this._sourceFeature = new ol_source_Vector ({ features: options.features || [] });
+  }
 
   ol_source_BinBase.call(this, options);
 };
 ol_ext_inherits(ol_source_FeatureBin, ol_source_BinBase);
 
-/** Set grid size
+/** Set features to use as bin collector
  * @param {ol.Feature} features
  */
 ol_source_FeatureBin.prototype.setFeatures = function (features) {
