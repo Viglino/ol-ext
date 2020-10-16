e8b38b650dcad31059128898daa06a27ce5cd54c
Viglino <jean-marc.viglino@ign.fr>
2020-10-16 16:46:56 +0200

    [BUILD]

--- dist/ol-ext.js
+++ dist/ol-ext.js
@@ -15184,6 +15184,7 @@ ol.interaction.FocusMap.prototype.setMap = function(map) {
   }
 };
 
+// 
 /** Interaction to draw on the current geolocation
  *	It combines a draw with a ol.Geolocation
  * @constructor
@@ -25815,6 +25816,151 @@ ol.geom.LineString.prototype.calcCSpline_ = function(options)
 }
 //NB: (Not confirmed)To use this module, you just have to :
 //   import('ol-ext/utils/cspline')
+/** Create a cardinal spline version of this geometry.
+ *	Original https://github.com/epistemex/cardinal-spline-js
+ *	@see https://en.wikipedia.org/wiki/Cubic_Hermite_spline#Cardinal_spline
+ *
+ * @param {} options
+ *	@param {Number} options.tension a [0,1] number / can be interpreted as the "length" of the tangent, default 0.5
+ *  @param {Number} options.resolution size of segment to split
+ *	@param {Integer} options.pointsPerSeg number of points per segment to add if no resolution is provided, default add 10 points per segment
+ */
+/** Cache cspline calculation on a geometry
+ * @param {} options
+ *	@param {Number} options.tension a [0,1] number / can be interpreted as the "length" of the tangent, default 0.5
+ *  @param {Number} options.resolution size of segment to split
+ *	@param {Integer} options.pointsPerSeg number of points per segment to add if no resolution is provided, default add 10 points per segment
+ * @return {ol.geom.Geometry}
+ */
+ol.geom.Geometry.prototype.cspline = function(options){
+  // Calculate cspline
+  if (this.calcCSpline_){
+    if (this.csplineGeometryRevision != this.getRevision() 
+      || this.csplineOption != JSON.stringify(options)) {
+      this.csplineGeometry_ = this.calcCSpline_(options)
+      this.csplineGeometryRevision = this.getRevision();
+      this.csplineOption = JSON.stringify(options);
+    }
+    return this.csplineGeometry_;
+  } else {	
+    // Default do nothing
+    return this;
+  }
+};
+ol.geom.GeometryCollection.prototype.calcCSpline_ = function(options) {
+  var g=[], g0=this.getGeometries();
+  for (var i=0; i<g0.length; i++) {
+    g.push(g0[i].cspline(options));
+  }
+  return new ol.geom.GeometryCollection(g);
+};
+ol.geom.MultiLineString.prototype.calcCSpline_ = function(options) {
+  var g=[], lines = this.getLineStrings();
+  for (var i=0; i<lines.length; i++) {
+    g.push(lines[i].cspline(options).getCoordinates());
+  }
+  return new ol.geom.MultiLineString(g);
+};
+ol.geom.Polygon.prototype.calcCSpline_ = function(options){
+  var g=[], g0=this.getCoordinates();
+  for (var i=0; i<g0.length; i++){
+    g.push((new ol.geom.LineString(g0[i])).cspline(options).getCoordinates());
+  }
+  return new ol.geom.Polygon(g);
+};
+ol.geom.MultiPolygon.prototype.calcCSpline_ = function(options) {
+  var g=[], g0=this.getPolygons();
+  for (var i=0; i<g0.length; i++) {
+    g.push(g0[i].cspline(options).getCoordinates());
+  }
+  return new ol.geom.MultiPolygon(g);
+}
+/** Calculate cspline on coordinates
+ * @param {Array<ol.geom.Geometry.coordinate>} line
+ * @param {} options
+ *	@param {Number} options.tension a [0,1] number / can be interpreted as the "length" of the tangent, default 0.5
+ *  @param {Number} options.resolution size of segment to split
+ *	@param {Integer} options.pointsPerSeg number of points per segment to add if no resolution is provided, default add 10 points per segment
+ * @return {Array<ol.geom.Geometry.coordinate>}
+ */
+ol.coordinate.cspline = function(line, options) {
+  if (!options) options={};
+  var tension = typeof options.tension === "number" ? options.tension : 0.5;
+  var length = 0;
+  var p0 = line[0];
+  line.forEach(function(p) {
+    length += ol.coordinate.dist2d(p0, p);
+    p0 = p;
+  })
+  var resolution = options.resolution || (length / line.length / (options.pointsPerSeg || 10));
+  var pts, res = [],			// clone array
+    x, y,					// our x,y coords
+    t1x, t2x, t1y, t2y,		// tension vectors
+    c1, c2, c3, c4,			// cardinal points
+    st, t, i;				// steps based on num. of segments
+  // clone array so we don't change the original
+  //
+  pts = line.slice(0);
+  // The algorithm require a previous and next point to the actual point array.
+  // Check if we will draw closed or open curve.
+  // If closed, copy end points to beginning and first points to end
+  // If open, duplicate first points to befinning, end points to end
+  if (line.length>2 && line[0][0]==line[line.length-1][0] && line[0][1]==line[line.length-1][1]) {
+    pts.unshift(line[line.length-2]);
+    pts.push(line[1]);
+  } else {
+    pts.unshift(line[0]);
+    pts.push(line[line.length-1]);
+  }
+  // ok, lets start..
+  function dist2d(x1, y1, x2, y2) {
+    var dx = x2-x1;
+    var dy = y2-y1;
+    return Math.sqrt(dx*dx+dy*dy);
+  }
+  // 1. loop goes through point array
+  // 2. loop goes through each segment between the 2 pts + 1e point before and after
+  for (i=1; i < (pts.length - 2); i++) {
+    var d1 = dist2d (pts[i][0], pts[i][1], pts[i+1][0], pts[i+1][1]);
+    var numOfSegments = Math.round(d1/resolution);
+    var d=1;
+    if (options.normalize) {
+      d1 = dist2d (pts[i+1][0], pts[i+1][1], pts[i-1][0], pts[i-1][1]);
+      var d2 = dist2d (pts[i+2][0], pts[i+2][1], pts[i][0], pts[i][1]);
+      if (d1<d2) d = d1/d2;
+      else d = d2/d1;
+    }
+    // calc tension vectors
+    t1x = (pts[i+1][0] - pts[i-1][0]) * tension *d;
+    t2x = (pts[i+2][0] - pts[i][0]) * tension *d;
+    t1y = (pts[i+1][1] - pts[i-1][1]) * tension *d;
+    t2y = (pts[i+2][1] - pts[i][1]) * tension *d;
+    for (t=0; t <= numOfSegments; t++) {
+      // calc step
+      st = t / numOfSegments;
+      // calc cardinals
+      c1 =   2 * Math.pow(st, 3) 	- 3 * Math.pow(st, 2) + 1; 
+      c2 = -(2 * Math.pow(st, 3)) + 3 * Math.pow(st, 2); 
+      c3 = 	   Math.pow(st, 3)	- 2 * Math.pow(st, 2) + st; 
+      c4 = 	   Math.pow(st, 3)	- 	  Math.pow(st, 2);
+      // calc x and y cords with common control vectors
+      x = c1 * pts[i][0]	+ c2 * pts[i+1][0] + c3 * t1x + c4 * t2x;
+      y = c1 * pts[i][1]	+ c2 * pts[i+1][1] + c3 * t1y + c4 * t2y;
+      //store points in array
+      if (x && y) res.push([x,y]);
+    }
+  }
+  return res;
+};
+/** @private */
+ol.geom.LineString.prototype.calcCSpline_ = function(options) {
+  var line = this.getCoordinates();
+  var res = ol.coordinate.cspline(line, options)
+  return new ol.geom.LineString(res);
+}
+// To use this module with ol/geom/Geometry
+// 
+
 /*	Copyright (c) 2017 Jean-Marc VIGLINO, 
 	released under the CeCILL-B license (French BSD license)
 	(http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt).
@@ -27170,6 +27316,9 @@ ol.style.FlowLine = function(options) {
   if (!options) options = {};
   ol.style.Style.call (this, { 
     renderer: this._render.bind(this),
+    stroke: options.stroke,
+    text: options.text,
+    zIndex: options.zIndex,
     geometry: options.geometry
   });
   // Draw only visible
@@ -27336,7 +27485,7 @@ ol.style.FlowLine.prototype.drawArrow = function (ctx, p0, p1, width, ratio) {
   var l = ol.coordinate.dist2d(p0, p1);
   var dx = (p0[0]-p1[0])/l;
   var dy = (p0[1]-p1[1])/l;
-  var width = Math.max(this.getArrowSize()[1]/2, width/2) * ratio;
+  width = Math.max(this.getArrowSize()[1]/2, width/2) * ratio;
   ctx.beginPath();
   ctx.moveTo(p0[0],p0[1]);
   ctx.lineTo(p0[0]-asize*dx+width*dy, p0[1]-asize*dy-width*dx);
@@ -27369,14 +27518,14 @@ ol.style.FlowLine.prototype._render = function(geom, e) {
       if (this.getOffset(1)) this._splitAsize(geom, this.getOffset(1) * e.pixelRatio, true)
       // Arrow 1
       if (geom.length>1 && (this.getArrow()===-1 || this.getArrow()===2)) {
-        var p = this._splitAsize(geom, asize);
+        p = this._splitAsize(geom, asize);
         if (this._acolor) ctx.fillStyle = this._acolor;
         else ctx.fillStyle = this.getColor(e.feature, 0);
         this.drawArrow(ctx, p[0], p[1], this.getWidth(e.feature, 0), e.pixelRatio);
       }
       // Arrow 2 
       if (geom.length>1 && this.getArrow()>0) {
-        var p = this._splitAsize(geom, asize, true);
+        p = this._splitAsize(geom, asize, true);
         if (this._acolor) ctx.fillStyle = this._acolor;
         else ctx.fillStyle = this.getColor(e.feature, 1);
         this.drawArrow(ctx, p[0], p[1], this.getWidth(e.feature, 1), e.pixelRatio);
