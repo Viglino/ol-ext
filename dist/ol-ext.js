5aeba77f299750384b8a5f51dd1e47caf7334381
Viglino <jean-marc.viglino@ign.fr>
2020-12-31 13:38:37 +0100

    [BUILD]

--- dist/ol-ext.js
+++ dist/ol-ext.js
@@ -13761,15 +13761,83 @@ ol.filter.PencilSketch.prototype.postcompose = function(e) {
   bwimg.width = w;
   bwimg.height = h;
   var bwctx = bwimg.getContext('2d');
-  bwctx.filter = 'invert(1) blur('+this.get('blur')+'px)';
-  bwctx.drawImage(canvas, 0,0, w, h);
+  bwctx.filter = 'grayscale(1) invert(1) blur('+this.get('blur')+'px)';
+  bwctx.drawImage(canvas, 0,0);
   ctx.save();
-    ctx.filter = 'grayscale(1)';
-    ctx.drawImage(canvas, 0,0, w, h);
-    ctx.filter = '';
+    if (!this.get('color')) {
+      ctx.filter = 'grayscale(1)';
+      ctx.drawImage(canvas, 0,0);
+    } else {
+      ctx.globalCompositeOperation = 'darken';
+      ctx.globalAlpha = .3;
+      ctx.drawImage(canvas, 0,0);
+    }
     ctx.globalCompositeOperation = 'color-dodge';
     ctx.globalAlpha = this.get('intensity');
     ctx.drawImage(bwimg, 0,0);
+  ctx.restore();
+};
+
+/*	Copyright (c) 2016 Jean-Marc VIGLINO, 
+  released under the CeCILL-B license (French BSD license)
+  (http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt).
+*/
+/** @typedef {Object} FilterPencilSketchOptions
+ * @property {number} blur blur value in pixel, default 8
+ * @property {number} value intensity value [0,1], default .8
+ */
+/** Colorize map or layer
+ * Original idea: https://www.freecodecamp.org/news/sketchify-turn-any-image-into-a-pencil-sketch-with-10-lines-of-code-cf67fa4f68ce/
+ * @constructor
+ * @requires ol.filter
+ * @extends {ol.filter.Base}
+ * @param {FilterPencilSketchOptions} options
+ */
+ol.filter.Pointillism = function(options) {
+  options = options || {};
+  ol.filter.Base.call(this, options);
+  this.set('blur', options.blur || 8);
+  this.set('intensity', options.intensity || .8);
+  this.pixels = [];
+  for (var i=0; i<1000000; i++) {
+    this.pixels.push([Math.random(), Math.random(), Math.random()*4+2]);
+  }
+};
+ol.ext.inherits(ol.filter.Pointillism, ol.filter.Base);
+/** @private 
+ */
+ol.filter.Pointillism.prototype.precompose = function(/* e */) {
+};
+/** @private 
+ */
+ol.filter.Pointillism.prototype.postcompose = function(e) {
+  // Set back color hue
+  var ctx = e.context;
+  var canvas = ctx.canvas;
+  var w = canvas.width;
+  var h = canvas.height;
+  // Grayscale image
+  var img = document.createElement('canvas');
+  img.width = w;
+  img.height = h;
+  var ictx = img.getContext('2d');
+  ictx.filter = 'saturate(400%)';
+  ictx.drawImage(canvas, 0,0, w, h);
+  ctx.save();
+    ctx.filter = 'blur(3px) saturate(200%)';
+    ctx.drawImage(canvas, 0,0);
+    //ctx.clearRect(0,0,w,h)
+    ctx.filter = 'none';
+    for (var i=0; i<w*h/50 && i<1000000; i++) {
+      var x = Math.floor(this.pixels[i][0]*w);
+      var y = Math.floor(this.pixels[i][1]*h);
+      var px = ictx.getImageData(x, y, 1, 1).data;
+      ctx.fillStyle = ol.color.asString(px);
+      ctx.opacity = .5;
+      ctx.beginPath();
+      ctx.arc(x,y,this.pixels[i][2],0, 2*Math.PI);
+      ctx.fill();
+    }
   ctx.restore();
 };
 
@@ -23224,6 +23292,64 @@ ol.source.Overpass.prototype.hasFeature = function(feature) {
     }
 	}
 	return false;
+};
+
+/*	Copyright (c) 2019 Jean-Marc VIGLINO,
+  released under the CeCILL-B license (French BSD license)
+  (http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.txt).
+*/
+/** Pointillism source: turn source into pointillism painting
+ * @constructor
+ * @extends {ol.source.Vector}
+ * @param {Object} options
+ *  @package {Array<ol/source/Source|ol/layer/Layer>} sources Input sources or layers. For vector data, use an VectorImage layer.
+ */
+ol.source.Pointillism = function (options) {
+  options.operation = this._operation;
+  options.operationType = 'image';
+  ol.source.Raster.call(this, options);
+  this.set('radius', options.radius || 4);
+  this.set('intensity', options.intensity || 25);
+  this.on('beforeoperations', function (event) {
+      console.log(event, options)
+    var w = Math.round((event.extent[2]-event.extent[0])/event.resolution);
+    var h = Math.round((event.extent[3]-event.extent[1])/event.resolution);
+    event.data.image = new ImageData(w,h);
+    event.data.radius = Number(this.get('radius')) || 1;
+    event.data.intensity = Number(this.get('intensity'));
+  }.bind(this));
+};
+ol.ext.inherits(ol.source.Pointillism, ol.source.Raster);
+/** Set value and force change
+ */
+ol.source.Pointillism.prototype.set = function(key, val) {
+  if (val) {
+    switch (key) {
+      case 'intensity': 
+      case 'radius': {
+        val = Number(val);
+        if (val<1) val = 1;
+        this.changed();
+        break;
+      }
+    }
+  }
+  return ol.source.Raster.prototype.set.call(this, key, val);
+};
+/**
+ * @private
+ */
+ol.source.Pointillism.prototype._operation = function(pixels, data) {
+  var width = pixels[0].width,
+    height = pixels[0].height,
+    imgData = pixels[0],
+    pixData = imgData.data,
+    pixelIntensityCount = [];
+  var destImageData = data.image,
+    destPixData = destImageData.data,
+    intensityLUT = [],
+    rgbLUT = [];
+  return pixels[0];
 };
 
 (function () {
