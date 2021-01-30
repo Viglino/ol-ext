fb7c9734afac8b7748b4fa537a421f04481163d6
Viglino <jean-marc.viglino@ign.fr>
2021-01-30 09:13:32 +0100

    [ADD] separate code/html

---
+++ examples/layer/map.layer.geoportail.wfs.js
@@ -0,0 +1,234 @@
+
+String.prototype.capitalize = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+var minZoom = 15;
+// The map
+var map = new ol.Map ({
+  target: 'map',
+  view: new ol.View ({
+    zoom: minZoom,
+    center: [261204.43490751847, 6250258.191535994]
+  })
+});
+
+var switcher = new ol.control.LayerSwitcher();
+map.addControl(switcher);
+map.addControl(new ol.control.Permalink({ visible: false }));
+map.addControl(new ol.control.ScaleLine());
+map.addControl(new ol.control.SearchBAN({
+  zoomOnSelect: 15
+}))
+
+map.addLayer(new ol.layer.Geoportail({
+  layer: 'ORTHOIMAGERY.ORTHOPHOTOS',
+  visible: true
+}));
+map.addLayer (new ol.layer.Geoportail({
+  layer: 'GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2', 
+  visible: false
+}));
+
+var loadLayer = new ol.layer.Vector({
+  title: 'chargement',
+  source: new ol.source.Vector(),
+  style: new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: [0,192,255,.5],
+      width: 1
+    })
+  })
+})
+map.addLayer(loadLayer);
+
+var vectorSource;
+var vectorLayer = new ol.layer.Vector({
+  title: 'WFS-IGN',
+  maxResolution: 10,  // prevent load on small zoom 
+  style: ol.style.geoportailStyle('BDTOPO_V3:troncon_de_route'),
+  declutter: true
+})
+map.addLayer(vectorLayer);
+
+function setWFS() {
+  loadLayer.getSource().clear();
+  if (vectorSource) vectorSource.clear();
+  popup.hide();
+  var type = $('#typename').val();
+  vectorLayer.set('title', type.split(':')[1].replace(/_/g,' ').capitalize());
+  switcher.drawPanel();
+  minZoom = /bati/.test(type) ? 16 : 15;
+  /* Standard WFS
+  vectorSource = new ol.source.Vector({
+    format: new ol.format.GeoJSON(),
+    url: function(extent) {
+      return 'https://wxs.ign.fr/choisirgeoportail/geoportail/wfs?service=WFS&' +
+        'version=1.1.0&request=GetFeature&' +
+        'typename='+type+'&' +
+        'outputFormat=application/json&srsname=EPSG:3857&' +
+        'bbox=' + extent.join(',') + ',EPSG:3857';
+    },
+    //strategy: ol.loadingstrategy.bbox
+    strategy: ol.loadingstrategy.tile(ol.tilegrid.createXYZ({ minZoom: minZoom, maxZoom: minZoom, tileSize:512  }))
+  });
+  */
+  // Loading bar
+  var loading = 0, loaded = 0;
+  var progressbar = document.getElementById('progressbar');
+  var draw = function() {
+    if (loading === loaded) {
+      loading = loaded = 0;
+      ol.ext.element.setStyle(progressbar, { width: 0 });// layer.layerswitcher_progress.width(0);
+      $('#loading').hide();
+    } else {
+      ol.ext.element.setStyle(progressbar, { width: (loaded / loading * 100).toFixed(1) + '%' });// layer.layerswitcher_progress.css('width', (loaded / loading * 100).toFixed(1) + '%');
+      $('#loading').show();
+      $('#loading span').text(loaded+'/'+loading)
+    }
+  }
+  var format = new ol.format.GeoJSON();
+  var source = vectorSource = new ol.source.Vector({
+    loader: function (extent, resolution, projection) {
+      loading++;
+      draw();
+      $.ajax({
+        url: 'https://wxs.ign.fr/choisirgeoportail/geoportail/wfs?service=WFS&' +
+          'version=1.1.0&request=GetFeature&' +
+          'typename='+type+'&' +
+          'outputFormat=application/json&srsname=EPSG:3857&' +
+          'bbox=' + extent.join(',') + ',EPSG:3857',
+        dataType: 'json',
+        success: function (response) {
+          if (response.error) {
+            alert(
+              response.error.message + '\n' + response.error.details.join('\n')
+            );
+          } else {
+            var features = format.readFeatures(response, {
+              featureProjection: projection,
+            });
+            if (features.length > 0) {
+              source.addFeatures(features);
+            }
+          }
+        },
+        complete: function () {
+          loaded++;
+          draw();
+          var f = new ol.Feature(ol.geom.Polygon.fromExtent(extent));
+          loadLayer.getSource().addFeature(f);
+        }
+      });
+    },
+    strategy: ol.loadingstrategy.tile(ol.tilegrid.createXYZ({ minZoom: minZoom, maxZoom: minZoom, tileSize:512  }))
+  });
+  vectorLayer.setSource(vectorSource);
+  vectorLayer.setMinZoom(minZoom);
+  vectorLayer.setStyle(ol.style.geoportailStyle(type, { sens : true, section: true }))
+  testZoom();
+}
+
+map.on('moveend', testZoom)
+function testZoom() {
+  $('#curZoom').text(map.getView().getZoom().toFixed(1)+' / '+minZoom);
+  if (map.getView().getZoom() <= minZoom) $('#zoomTo').show();
+  else $('#zoomTo').hide();
+}
+
+// Chargement d'une commune
+var commune = new ol.source.Vector();
+map.addLayer(new ol.layer.Vector({
+  title: 'Communes',
+  source: commune,
+  style: new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: [255,0,255,.7],
+      width: 5,
+      lineDash: [0, 15, 15, 15]
+    })
+  })
+}));
+$('#commune form').on('submit', function(e) {
+  e.preventDefault();
+  $('#commune').addClass('hidden');
+  var code = $('#commune input').val();
+  if (code) {
+    $.ajax({
+      url: 'https://geo.api.gouv.fr/communes?code='+code+'&fields=nom,code,contour&format=geojson&geometry=contour',
+      success: function(data) {
+        var f = new ol.format.GeoJSON().readFeatures(data, { featureProjection: map.getView().getProjection() });
+        commune.addFeatures(f);
+        map.getView().fit(f[0].getGeometry().getExtent())
+        if (map.getView().getZoom()>15) map.getView().setZoom(15)
+      }
+    })
+  }
+})
+
+// Selection
+var sel = new ol.interaction.Select({
+  multi: true,
+  layers: [vectorLayer],
+  condition: ol.events.condition.click,
+  hitTolerance: 3
+})
+map.addInteraction(sel);
+sel.on('select', function(e) {
+  var f = e.selected[0];
+  if (f) console.log(f.getProperties());
+})
+
+// Popup
+var popup = new ol.Overlay.PopupFeature({
+  popupClass: 'default anim',
+  closeBox: true,
+  select: sel,
+  canFix: true,
+  template: {
+    title: function(feature) {
+      return vectorLayer.get('title');
+    },
+    attributes: [
+      'nature', 'nom_1_droite', 'code_postal_droit', 'etat_de_l_objet', 'urbain',
+      'cpx_classement_administratif', 'cpx_numero',
+      'usage_1', 'origine_du_batiment',
+      'nom_com', 'code_insee', 'code_arr', 'section', 'numero'
+    ]
+  }
+});
+map.addOverlay(popup)
+
+setWFS();
+
+// Save Vector layer
+function save() {
+  var format = new ol.format.GeoJSON();
+  var features = vectorSource.getFeatures();
+  if (features.length) {
+    var data = format.writeFeatures(features, {
+      dataProjection: 'EPSG:4326',
+      featureProjection: map.getView().getProjection()
+    });
+    var blob = new Blob([data], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, "map.geojson");
+  }
+  // commune
+  features = commune.getFeatures();
+  if (features.length) {
+    if (features.length === 1) {
+      var geom = ol.geom.Polygon.fromExtent(ol.extent.buffer(features[0].getGeometry().getExtent(), 1000));
+      geom = geom.getCoordinates();
+      features[0].getGeometry().getCoordinates().forEach(function(p) {
+        geom.push(p);
+      });
+      features.push(new ol.Feature(new ol.geom.Polygon(geom)))
+    }
+    var data = format.writeFeatures(features, {
+      dataProjection: 'EPSG:4326',
+      featureProjection: map.getView().getProjection()
+    });
+    var blob = new Blob([data], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, "commune.geojson");
+  }
+}
---
+++ examples/layer/map.layer.gppvtile.js
@@ -0,0 +1,430 @@
+﻿var perma = new ol.control.Permalink();
+if (perma.getUrlParam('embed')) {
+  $('body > a').hide();
+  $('.info').hide();
+  $('#map').css('top', 0);
+  $('.options').css('top', 0);
+}
+
+// Color
+var onChangeColor = function() {}
+$('#color input').spectrum({
+  flat: true,
+  localStorageKey: 'spectrum',
+  showAlpha: false,
+  showPalette: true,
+  showInitial: true,
+  showInput: true,
+  chooseText: "choisir",
+  cancelText: "annuler",
+  preferredFormat: "hex3",
+  clickoutFiresChange: true,
+  change: function(color) {
+    console.log('change')
+    onChangeColor(color.toHexString());
+    onChangeColor = function() {};
+  }
+});
+
+// The map
+var map = new ol.Map({
+  target: 'map',
+  view: new ol.View ({
+    zoom: 15,
+    center: [261204.43490751847, 6250258.191535994]
+  }),
+  interactions: ol.interaction.defaults(),
+  // layers: [ new ol.layer.Geoportail('ORTHOIMAGERY.ORTHOPHOTOS')]
+});
+map.addControl(perma);
+
+var vlayer = new ol.layer.VectorTile({
+  title: "Plan IGN vecteur",
+  renderMode: 'hybrid',
+  source: new ol.source.VectorTile({
+    tilePixelRatio: 1,
+    tileGrid: ol.tilegrid.createXYZ({ maxZoom: 19 }),
+    format: new ol.format.MVT(),
+    projection: new ol.proj.Projection({code:"EPSG:3857"}),
+    //url: "https://vectortiles.ign.fr/rok4server/1.0.0/PLAN.IGN/{z}/{x}/{y}.pbf",
+    url : "https://wxs.ign.fr/an7nvfzojv5wa96dsga5nk8w/geoportail/tms/1.0.0/PLAN.IGN/{z}/{x}/{y}.pbf",
+    // url: "https://vectortiles.ign.fr/rok4server/1.0.0/PLAN.IGN/{z}/{x}/{y}.pbf",
+    attributions: '<a href="https://geoservices.ign.fr/blog/2018/07/08/nouveautes_vecteur.html">&copy; IGN-Géoportail</a>',
+  }),
+  declutter: true
+});
+map.addLayer(vlayer);
+
+// Lecture du fichier de style
+var globalStyle, baseStyles={}, currentStyle;
+ol.ext.Ajax.get({
+  //url: './styles/planign.json',
+  url: 'https://wxs.ign.fr/choisirgeoportail/static/vectorTiles/styles/PLAN.IGN/standard.json',
+  // url: 'https://wxs.ign.fr/an7nvfzojv5wa96dsga5nk8w/static/vectorTiles/styles/PLAN.IGN/classique.json',
+  // url: 'https://vectortiles.ign.fr/demonstrateur/styles/planign.json',
+  //url: 'http://calac-4.ign.fr/pyramide_ecran/style_mapbox.json',
+  // url: 'https://vectortiles.ign.fr/demonstrateur/styles/gris.json',
+  // url: 'https://vectortiles.ign.fr/demonstrateur/styles/muet.json',
+  success: function(style) {
+    /* add sens circu * /
+    style.layers.push(rdirect);
+    style.layers.push(rinvers);
+    /**/
+    globalStyle = currentStyle = style;
+    olms.applyStyle(vlayer, style, 'plan_ign').then(function () {});
+    showLayers();
+  }
+});
+
+// Load base styles
+['standard', 'gris', 'accentue', 'attenue', 'classique'].forEach(function(s) {
+  ol.ext.Ajax.get({
+    url: 'https://wxs.ign.fr/choisirgeoportail/static/vectorTiles/styles/PLAN.IGN/'+s+'.json',
+    success: function(style) {
+      baseStyles[s] = style;
+      var sel = document.getElementById('styles');
+      ol.ext.element.create('OPTION', {
+        value: s,
+        html: s,
+        parent: sel
+      })
+    }
+  })
+});
+// Set base style
+function setBaseStyle(n) {
+  globalStyle = currentStyle = baseStyles[n];
+  $('.options .theme').change();
+  //olms.applyStyle(vlayer, currentStyle, "plan_ign")
+}
+
+// Apply color
+function setStyleColor(l, vis, color) {
+  l.layout.visibility = vis;
+  ['text-color', /*'text-halo-color',*/ 'fill-color', 'fill-outline-color', 'line-color', 'circle-color', 'circle-stroke-color', 'icon-color'].forEach(function(c) {
+    if (!l.paint[c]) return;
+    if (!l.savePaintColor) l.savePaintColor = {};
+    if (l.savePaintColor[c]) {
+      if (l.savePaintColor[c].stops) l.paint[c] = $.extend(true, {}, l.savePaintColor[c]);
+      else l.paint[c] = l.savePaintColor[c];
+    }
+    if (color && l.paint) {
+      if (!l.savePaintColor[c]) {
+        if (l.paint[c] && l.paint[c].stops) l.savePaintColor[c] = $.extend(true, {}, l.paint[c]);
+        else l.savePaintColor[c] = l.paint[c];
+      }
+      switch(color) {
+        case 'brighten':
+        case 'darken':
+        case 'brighten2':
+        case 'darken2':
+        case 'saturate':
+        case 'desaturate':
+        case 'saturate2':
+        case 'desaturate2':
+        case 'gray': {
+          var opt = parseInt(color.replace(/([^0-9])/g,'')) || .5;
+          var operation = color.replace(/[0-9]$/, '');
+          if (color==='gray') {
+            opt = 4;
+            operation = 'desaturate';
+          }
+          //console.log(operation,opt)
+          try {
+            if (l.paint[c].stops) {
+              l.paint[c].stops.forEach(function (s) {
+                s[1] = chroma(s[1])[operation](opt).hex(); 
+                // console.log('STOP:',s)
+              })
+            } else {
+              l.paint[c] = chroma(l.savePaintColor[c])[operation](opt).hex(); 
+            }
+          } catch(e){};
+          break; //'#cccccc'; break;
+        }
+        case 'red':
+        case 'green':
+        case 'blue':
+        default: {
+          try {
+            if (l.paint[c].stops) {
+              l.paint[c].stops.forEach(function (s) {
+                s[1] = chroma.mix(s[1],color).hex(); 
+              })
+            } else {
+              l.paint[c] = chroma.mix(l.savePaintColor[c],color).hex(); 
+            }
+          } catch(e){};
+          break; 
+        }
+      }
+      // console.log(l.savePaintColor, l.paint[l.type+'-color']);
+    }
+  })
+}
+
+function getVisibleStyle(check, color) {
+  var vis = $(check).prop('checked') ? 'visible' : 'hidden';
+  var style = currentStyle = $.extend({}, globalStyle);
+  style.layers = [];
+  var layer = $(check).data('layer');
+  globalStyle.layers.forEach(function(l) {
+    if (l.layout) {
+      if (l['source-layer'] === layer) setStyleColor(l, vis, color);
+      if (l.layout.visibility !== 'hidden') style.layers.push(l);
+    }
+  });
+  return style;
+}
+
+// Apply the layers style
+function showLayers() {
+  var sources = {};
+  globalStyle.layers.forEach(function(l) {
+    var name = l['source-layer'];
+    if (name) {
+      var theme = name.split('_').shift();
+      if (!sources[theme]) sources[theme] = [];
+      sources[theme][name] = l;
+    }
+  });
+  var ul = $('.options ul');
+  console.log(sources)
+  Object.keys(sources).forEach(function(s) {
+    var li = $('<li>').appendTo(ul);
+    var label = $('<label>').text(s).appendTo(li);
+    $('<input type="checkbox">')
+      .addClass('theme')
+      .prop('checked', true)
+      .on('change', function() {
+        var vis = $(this).prop('checked');
+        var style;
+        $('input', ul2).each(function() {
+          $(this).prop('checked', vis);//.change();
+          style = getVisibleStyle(this, color);
+        })
+        olms.applyStyle(vlayer, style, "plan_ign");
+      })
+      .prependTo(label);
+    var color;
+    $('<select>').html(`
+      <option value="">normal</option>
+      <option value="gray">gray</option>
+      <option value="saturate">saturate</option>
+      <option value="saturate2">saturate2</option>
+      <option value="desaturate">desaturate</option>
+      <option value="desaturate2">desaturate2</option>
+      <option value="darken">darken</option>
+      <option value="darken2">darken2</option>
+      <option value="brighten">brighten</option>
+      <option value="brighten2">brighten2</option>
+      <option value="red">red</option>
+      <option value="green">green</option>
+      <option value="blue">blue</option>
+      <option value="color" style="display: none;">user color</option>
+      <option value="color">user color</option>
+      `)
+      .on('change', function(){
+        color = $(this).val();
+        if (color==='color') {
+          /*
+          color = prompt('Enter a color code (#rrggbb):') || '';
+          if (color.charAt(0)!=='#') color = '#'+color;
+          if (!/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(color)) {
+            color = '';
+          }
+          $(this).val('color');
+          */
+          var self = this;
+          $(this).val('color');
+          console.log('ok')
+          $('#color').show();
+          $("#color .sp-cancel").off();
+          $("#color .sp-cancel").on('click', function() { $('#color').hide(); })
+          setTimeout(function() {
+            onChangeColor = function(c) {
+              color = c;
+              $(self).data('color', color);
+              $('input', ul2).each(function() {
+                $(this).change();
+              })
+              $('#color').hide();
+            }
+          })
+
+          /*
+          $("#color .sp-choose").off('click');
+          $("#color .sp-choose").on('click', function() { 
+            color = $('#color input').spectrum('get').toHexString();
+            $(self).data('color', color);
+            $('input', ul2).each(function() {
+              $(this).change();
+            })
+            $('#color').hide();
+          })
+          */
+          return;
+        }
+        $(this).data('color', color);
+        $('input', ul2).each(function() {
+          $(this).change();
+        })
+      })
+      .appendTo(li);
+    var ul2 = $("<ul>").appendTo(li);
+    Object.keys(sources[s]).forEach(function(i) {
+      var li = $('<li>').appendTo(ul2);
+      var source = sources[s][i];
+      var label = $('<label>').text(i.replace(/toponyme_|routier_|ocs_|hydro_|parcellaire_/, '')).attr('title', i).appendTo(li);
+      $('<input type="checkbox">')
+        .prop('checked', source.layout.visibility!=='hidden' ? 'checked':'')
+        .data('layer', i)
+        .on('change', function() {
+          var style = getVisibleStyle(this, color);
+          olms.applyStyle(vlayer, style, "plan_ign");
+        })
+        .prependTo(label);
+    });
+  });
+}
+
+// Selection
+var selStyle = ol.style.Style.defaultStyle(true)
+selStyle.push(new ol.style.Style({
+  image: new ol.style.Circle({
+    radius: 3,
+    fill: new ol.style.Fill({ color: '#f00' })
+  }),
+  geometry: function(f) {
+    return new ol.geom.Point(f.getGeometry().getFirstCoordinate())
+  }
+}))
+var select = new ol.layer.Vector({ 
+  source: new ol.source.Vector(),
+  style: selStyle
+});
+map.addLayer(select);
+
+map.on('click', function() {
+  var f = select.getSource().getFeatures()[0];
+  if (f) console.log(f.getProperties())
+})
+
+var tooltip  = new ol.Overlay.Tooltip({ className: 'default', positioning: 'bottom-center' });
+map.addOverlay(tooltip);
+
+var hover = new ol.interaction.Hover({ 
+  cursor: "pointer",
+  layers: [vlayer]
+  });
+map.addInteraction(hover);
+hover.on("enter", function(e) {
+  var showGeom = $('#showSel').prop('checked');
+  // hover.setCursor("pointer");
+  var feature = e.feature;
+  if (select.getSource().getFeatures().length) select.getSource().clear();
+  var info = '';
+  if (feature && feature.get('layer')!=='fond_opaque') {
+    info = '<h2>'+feature.get('layer').replace(/_/g,' ')+'</h2>';
+    ['symbo', 'nature', 'rond_point', 'sens_circu', 'texte', 'nom_desabrege', 'numero', 'hauteur'].forEach(function(a) {
+      if (feature.get(a)) info += '<br/>'+a.replace(/_/g,' ')+': '+feature.get(a);
+    })
+  }
+  tooltip.setInfo(info);
+  //console.log(feature.getProperties());
+  /* Select feature */
+  if (showGeom) {
+    var coords = [];
+    if (feature instanceof ol.Feature) {
+      select.getSource().addFeature(feature);
+    } else {
+      var c = feature.getFlatCoordinates();
+      for (var i=0; i<c.length; i+=2) {
+        coords.push ([c[i],c[i+1]]);
+      }
+      // console.log(feature.getType())
+      switch (feature.getType()) {
+        case 'Point': {
+          coords = coords.pop();
+          break;
+        }
+        case 'LineString' : {
+          coords = coords;
+          break;
+        }
+        case 'MultiLineString' : 
+        case 'Polygon' : {
+          coords = [coords];
+          break;
+        }
+      }
+      var geom = new ol.geom[feature.getType()](coords)
+      var f2 = new ol.Feature(geom);
+      f2.setProperties(feature.getProperties())
+      select.getSource().addFeature(f2);
+    }
+  }
+  /**/
+});
+hover.on("leave", function(e) {
+  tooltip.setInfo('');
+  select.getSource().clear();
+});
+
+
+function getLayerStyle(layer) {
+  var styles = [];
+  currentStyle.layers.forEach(function(style) {
+    if (style['source-layer']===layer) {
+      styles.push(style);
+    }
+  })
+  return styles;
+}
+
+// Reset styles
+function reset() {
+  $('.options ul select').val('').change();
+  $('.options .theme').each(function() {
+    $(this).prop('checked', true).change()
+  });
+}
+
+function save() {
+  var data = JSON.stringify(currentStyle);
+  var blob = new Blob([data], {type: "text/plain;charset=utf-8"});
+  saveAs(blob, "custom.json");
+}
+
+var sens = {
+  "id": "Routier - sens direct",
+  "type": "symbol",
+  "source": "plan_ign",
+  "source-layer": "routier_route",
+  "filter": [
+    "==",
+    "sens_circu",
+    "Sens direct"
+  ],
+  "layout": {
+    "visibility": "visible",
+    "text-field": "→",
+    "symbol-placement": "line",
+    "text-size": 15,
+    "text-anchor": "center",
+    "text-keep-upright": false,
+    "text-rotation-alignment":"map",
+    "text-pitch-alignment": "viewport",
+  },
+  "paint": {
+    "text-color": "#000000"
+  }
+}
+var rdirect = $.extend(true, {}, sens);
+rdirect.id = 'Routier - sens direct';
+rdirect.layout['text-field'] = '→';
+rdirect.filter[2] = "Sens direct";
+var rinvers = $.extend(true, {}, sens);
+rinvers.id = 'Routier - sens inverse'
+rinvers.layout['text-field'] = '←'
+rinvers.filter[2] = "Sens inverse";
