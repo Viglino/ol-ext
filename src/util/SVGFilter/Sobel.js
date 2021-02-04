804f874b2a5fdf359bca090d8fe3f2d1a27627b1
Viglino <jean-marc.viglino@ign.fr>
2021-02-04 14:53:14 +0100

    [ADD] SVG filters

--- src/util/SVGFilter/Sobel.js
+++ src/util/SVGFilter/Sobel.js
@@ -5,7 +5,6 @@
 
 import ol_ext_inherits from '../util/ext'
 import ol_ext_SVGFilter from '../../util/SVGFilter'
-import ol_ext_SVGOperation from '../../util/SVGOperation';
 
 /** Apply a sobel filter on an image
  * @constructor
@@ -18,112 +17,75 @@ import ol_ext_SVGOperation from '../../util/SVGOperation';
 var ol_ext_SVGFilter_Sobel = function(options) {
   options = options || {};
 
-  ol_ext_SVGFilter.call(this);
+  ol_ext_SVGFilter.call(this, { id: options.id, color: 'sRGB' });
 
-  // Red channel
-  this._addColorSobel('red');
-  // Green channel
-  this._addColorSobel('green');
-  // Blue channel
-  this._addColorSobel('blue');
-  // Combine
+  var operation = {
+    feoperation: 'feConvolveMatrix',
+    in: 'SourceGraphic',
+    preserveAlpha: true,
+    order: 3
+  };
+  // Vertical
+  operation.kernelMatrix = [
+    -1, -2, -1, 
+     0,  0,  0,
+     1,  2,  1
+  ];
+  operation.result = 'V1';
+  this.addOperation(operation);
+  operation.kernelMatrix = [
+     1,  2,  1, 
+     0,  0,  0,
+    -1, -2, -1
+  ];
+  operation.result = 'V2';
+  this.addOperation(operation);
+  // Horizontal
+  operation.kernelMatrix = [
+    -1,  0,  1, 
+    -2,  0,  2,
+    -1,  0,  1
+  ];
+  operation.result = 'H1';
+  this.addOperation(operation);
+  operation.kernelMatrix = [
+     1, -0, -1, 
+     2,  0, -2,
+     1,  0, -1
+  ];
+  operation.result = 'H2';
+  this.addOperation(operation);
+  // Compose V
   this.addOperation({
     feoperation: 'feComposite',
     operator: 'arithmetic',
-    in: 'rededge',
-    in2: 'greenedge',
-    k2: 1,
-    k3: 1
-  });
-  this.addOperation({
-    feoperation: 'feComposite',
-    operator: 'arithmetic',
-    in2: 'blueedge',
+    in: 'V1',
+    in2: 'V2',
     k2: 1,
     k3: 1,
-    result: 'finaledges'
+    result: 'V'
   });
-  if (options.grayscale) {
-    this.addOperation({
-      feoperation: 'feColorMatrix',
-      type: 'saturate',
-      values: 0
-    });
-  } else if (options.alpha) {
-    this.addOperation({
-      feoperation: 'feColorMatrix',
-      type: 'luminanceToAlpha'
-    });
-  }
+  // Compose H
+  this.addOperation({
+    feoperation: 'feComposite',
+    operator: 'arithmetic',
+    in: 'H1',
+    in2: 'H2',
+    k2: 1,
+    k3: 1,
+    result: 'H'
+  });
+  // Merge
+  this.addOperation({
+    feoperation: 'feBlend',
+    mode: 'lighten',
+    in: 'H',
+    in: 'V'
+  });
+  if (options.grayscale) this.grayscale();
+  else if (options.alpha) this.luminanceToAlpha();
+  if (options.gamma) this.ge
 };
 ol_ext_inherits(ol_ext_SVGFilter_Sobel, ol_ext_SVGFilter);
-
-/** Sobel filter on a color
- * @param {ol_ext_SVGFilter} filter
- * @param {string} color color name : red/green/blue
- * @private
- */
-ol_ext_SVGFilter_Sobel.prototype._addColorSobel = function(color) {
-  var r = (color==='red' ? 1:0);
-  var g = (color==='green' ? 1:0);
-  var b = (color==='blue' ? 1:0);
-  // Color channel
-  this.addOperation(new ol_ext_SVGOperation({
-    feoperation: 'feColorMatrix',
-    in: 'SourceGraphic',
-    type: 'matrix',
-    values:[
-      0, 0, 0, 0, 1,
-      0, 0, 0, 0, 1,
-      0, 0, 0, 0, 1,
-      r, g, b, 0, 0
-    ],
-    result: color+'Chan'
-  }));
-  // Horizontal 
-  this.addOperation(new ol_ext_SVGOperation({
-    feoperation: 'feConvolveMatrix',
-    in: color+'Chan',
-    order: 3,
-    kernelMatrix: [
-      -1, -2, -1,
-       0,  0,  0,
-       1,  2,  1],
-    result: color+'Hor'
-  }));
-  // Vertical
-  this.addOperation(new ol_ext_SVGOperation({
-    feoperation: 'feConvolveMatrix',
-    in: color+'Chan',
-    order: 3,
-    kernelMatrix: [
-      -1,  0,  1,
-      -2,  0,  2,
-      -1,  0,  1],
-    result: color+'Ver'
-  }));
-  // Combine
-  this.addOperation(new ol_ext_SVGOperation({
-    feoperation: 'feComposite',
-    operator: 'arithmetic',
-    k2: 1,
-    k3: 1,
-    in: color+'Hor',
-    in2: color+'Ver'
-  }));
-  // Edges
-  this.addOperation(new ol_ext_SVGOperation({
-    feoperation: 'feColorMatrix',
-    type: 'matrix',
-    values: [
-      0, 0, 0, r, 0,
-      0, 0, 0, g, 0,
-      0, 0, 0, b, 0,
-      0, 0, 0, 0, 1
-    ],
-    result: color+'edge'
-  }));
-};
-
 
 export default ol_ext_SVGFilter_Sobel
