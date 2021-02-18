2702f700b7aca7cfe2f6d0ed0df89603357d5b27
Viglino <jean-marc.viglino@ign.fr>
2021-02-18 16:29:57 +0100

    [ADD] set active

--- src/interaction/Hover.js
+++ src/interaction/Hover.js
@@ -52,6 +52,20 @@ ol_interaction_Hover.prototype.setMap = function(map) {
   ol_interaction_Interaction.prototype.setMap.call (this, map);
 };
 
+/** Activate / deactivate interaction
+ * @param {boolean} b
+ */
+ol_interaction_Hover.prototype.setActive = function(b) {
+  ol_interaction_Interaction.prototype.setActive.call (this, b);
+  if (this.cursor_) {
+    var style = map.getTargetElement().style;
+    if (this.previousCursor_ !== undefined) {
+      style.cursor = this.previousCursor_;
+      this.previousCursor_ = undefined;
+    }
+  }
+};
+
 /**
  * Set cursor on hover
  * @param { string } cursor css cursor propertie or a function that gets a feature, default: none
