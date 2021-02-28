d9e7dd33b19f238434b501d8df348c2b3cb962f9
Viglino <jean-marc.viglino@ign.fr>
2021-02-28 18:05:08 +0100

    [BUG]

--- dist/ol-ext.js
+++ dist/ol-ext.js
@@ -27214,7 +27214,7 @@ ol.Overlay.FixedPopup.prototype.setPixelPosition = function (pix, position) {
     else pix[0] = mapSize[0]/2 + pix[0];
   }
   if (pix) this._pixel = pix;
-  if (map) {
+  if (map && this._pixel) {
     this.updateRenderedPosition(this._pixel, mapSize);
     // Prevent outside
     var outside = false;
