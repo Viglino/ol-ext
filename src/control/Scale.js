ec4123c98dec84450039bdeeaaf6ce819f481a20
Viglino <jean-marc.viglino@ign.fr>
2021-04-04 11:47:25 +0200

    [ADD] set and getMapScale

--- src/control/Scale.js
+++ src/control/Scale.js
@@ -4,9 +4,9 @@
 */
 import ol_ext_inherits from '../util/ext'
 import {unByKey as ol_Observable_unByKey} from 'ol/Observable'
-import {getDistance as ol_sphere_getDistance} from 'ol/sphere'
-import {transform as ol_proj_transform} from 'ol/proj'
 import ol_control_Control from 'ol/control/Control'
+import {getMapScale as ol_sphere_getMapScale} from '../geom/sphere'
+import {setMapScale as ol_sphere_setMapScale} from '../geom/sphere'
 
 /**
  * Scale Control.
@@ -44,8 +44,6 @@ var ol_control_Scale = function(options) {
   this._input.addEventListener("change", this.setScale.bind(this));
   
   this.set('ppi', options.ppi || 96)
-
-
 };
 ol_ext_inherits(ol_control_Scale, ol_control_Control);
 
@@ -64,26 +62,18 @@ ol_control_Scale.prototype.setMap = function (map) {
 
   // Get change (new layer added or removed)
   if (map) {
-    this._listener = map.on('moveend', this._showScale.bind(this));
+    this._listener = map.on('moveend', this.getScale.bind(this));
   }
 };
 
 /** Display the scale
  */
-ol_control_Scale.prototype._showScale = function () {
+ol_control_Scale.prototype.getScale = function () {
   var map = this.getMap();
   if (map) {
-    var view = map.getView();
-    var proj = view.getProjection();
-    var center = view.getCenter();
-    var px = map.getPixelFromCoordinate(center);
-    px[1] += 1;
-    var coord = map.getCoordinateFromPixel(px);
-    var d = ol_sphere_getDistance(
-      ol_proj_transform(center, proj, 'EPSG:4326'),
-      ol_proj_transform(coord, proj, 'EPSG:4326'));
-    d *= this.get('ppi')/.0254
+    var d = ol_sphere_getMapScale(map, this.get('ppi'));
     this._input.value = this.formatScale(d);
+    return d;
   }
 };
 
@@ -104,27 +94,9 @@ ol_control_Scale.prototype.setScale = function (value) {
   var map = this.getMap();
   if (map && value) {
     if (value.target) value = value.target.value;
-    var fac = value;
-    if (typeof(value)==='string') {
-      fac = value.split('/')[1];
-      if (!fac) fac = value;
-      fac = fac.replace(/[^\d]/g,'');
-      fac = parseInt(fac);
-    }
-    // Calculate new resolution
-    var view = map.getView();
-    var proj = view.getProjection();
-    var center = view.getCenter();
-    var px = map.getPixelFromCoordinate(center);
-    px[1] += 1;
-    var coord = map.getCoordinateFromPixel(px);
-    var d = ol_sphere_getDistance(
-      ol_proj_transform(center, proj, 'EPSG:4326'),
-      ol_proj_transform(coord, proj, 'EPSG:4326'));
-    d *= this.get('ppi')/.0254
-    view.setResolution(view.getResolution()*fac/d);
+    ol_sphere_setMapScale(map, value, this.get('ppi'));
   }
-  this._showScale();
+  this.getScale();
 };
 
 export default ol_control_Scale
