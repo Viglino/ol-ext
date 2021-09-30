28d42cf9da9eaed3f33251f767c5c15496a6f15e
Viglino <jean-marc.viglino@ign.fr>
2021-09-30 14:49:56 +0200

    [UPD] add projection for extent convertion

--- src/layer/Geoportail.js
+++ src/layer/Geoportail.js
@@ -15,8 +15,9 @@ import ol_source_Geoportail from '../source/Geoportail'
  * @extends {ol.layer.Tile}
  * @param {olx.layer.WMTSOptions=} options WMTS options if not defined default are used
  *  @param {string} options.layer Geoportail layer name
- *  @param {string} options.gppKey Geoportail API key
- *  @param {olx.source.WMTSOptions=} tileoptions WMTS options if not defined default are used
+ *  @param {string} options.gppKey Geoportail API key, default use layer registered key
+ *  @param {ol.projectionLike} [options.projection=EPSG:3857] projection for the extent, default EPSG:3857
+ * @param {olx.source.WMTSOptions=} tileoptions WMTS options if not defined default are used
  */
 var ol_layer_Geoportail = function(layer, options, tileoptions) {
   options = options || {};
@@ -58,7 +59,8 @@ var ol_layer_Geoportail = function(layer, options, tileoptions) {
   if (!options.desc) options.desc = capabilities.desc;
   if (!options.extent && capabilities.bbox) {
     if (capabilities.bbox[0]>-170 && capabilities.bbox[2]<170) {
-      options.extent = ol_proj_transformExtent(capabilities.bbox, 'EPSG:4326', 'EPSG:3857');
+      options.extent = ol_proj_transformExtent(capabilities.bbox, 'EPSG:4326', options.projection || 'EPSG:3857');
+      this.set('extent', options.extent);
     }
   }
   options.maxZoom = maxZoom;
