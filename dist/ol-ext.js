c6690dd6439d84c6ee8c10467da679c5cd0d3fd9
viglino <jean-marc.viglino@ign.fr>
2022-01-10 14:03:20 +0100

    [BUILD]

--- dist/ol-ext.js
+++ dist/ol-ext.js
@@ -20328,6 +20328,10 @@ ol.interaction.Clip.prototype.precompose_ = function(e) {
       (pt[0]*tr[0] - pt[1]*tr[1] + tr[4]),
       (-pt[0]*tr[2] + pt[1]*tr[3] + tr[5])
     ];
+    // Vector layer
+    if (e.target.getSource().getFeatures) {
+      radius *= ratio;
+    }
   } else {
     pt[0] *= ratio;
     pt[1] *= ratio;
