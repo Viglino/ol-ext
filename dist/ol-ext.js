5f58850ad94a787704e37c5220f95fb77110a715
viglino <jean-marc.viglino@ign.fr>
2024-03-28 13:41:01 +0100

    [BUILD]

--- dist/ol-ext.js
+++ dist/ol-ext.js
@@ -34208,11 +34208,19 @@ ol.layer.AnimatedCluster = class ollayerAnimatedCluster extends ol.layer.Vector 
     this.animation = { start: false }
     this.set('animationDuration', typeof (options.animationDuration) == 'number' ? options.animationDuration : 700)
     this.set('animationMethod', options.animationMethod || ol.easing.easeOut)
-    // Save cluster before change
-    this.getSource().on('change', this.saveCluster.bind(this))
     // Animate the cluster
     this.on(['precompose', 'prerender'], this.animate.bind(this))
     this.on(['postcompose', 'postrender'], this.postanimate.bind(this))
+  }
+  /** Set the cluster source
+   * @param {ol.source.Vector} source
+   */
+  setSource(source) {
+    if (!this._saveClusterFn) this._saveClusterFn = this.saveCluster.bind(this)
+    // Save cluster before change
+    if (this.getSource()) this.getSource().un('change', this._saveClusterFn)
+    ol.layer.Vector.prototype.setSource.call(this, source)
+    if (this.getSource()) this.getSource().on('change', this._saveClusterFn)
   }
   /** save cluster features before change
    * @private
