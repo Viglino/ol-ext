f5e7a093a09ab19e0c36b3f5b4ecb9e76bc7c5a0
viglino <jean-marc.viglino@ign.fr>
2024-06-19 16:48:02 +0200

    [ADD] search coordinates

--- src/control/SearchCoordinates.js
+++ src/control/SearchCoordinates.js
@@ -17,22 +17,25 @@ import ol_ext_element from '../util/element.js'
  * @extends {ol_control_Search}
  * @fires select
  * @param {Object=} Control options. 
+ *  @param {ol/proj/ProjectionLike} options.projection 
  *  @param {string} [options.className] control class name
  *  @param {Element | string | undefined} [options.target] Specify a target if you want the control to be rendered outside of the map's viewport.
- *  @param {string | undefined} [options.label=search] Text label to use for the search button, default "search"
+ *  @param {string | undefined} [options.label="search"] Text label to use for the search button, default "search"
  *  @param {string | undefined} [options.labelGPS="Locate with GPS"] placeholder, default "Locate with GPS"
  *  @param {number | undefined} [options.typing=300] a delay on each typing to start searching (ms), default 300.
  *  @param {integer | undefined} [options.minLength=1] minimum length to start searching, default 1
  *  @param {integer | undefined} [options.maxItems=10] maximum number of items to display in the autocomplete list, default 10
  */
-var ol_control_SearchGPS = class olcontrolSearchGPS extends ol_control_Search {
+var ol_control_SearchCoordinates = class olcontrolSearchCoordinates extends ol_control_Search {
   constructor(options) {
     options = options || {};
-    options.className = (options.className || '') + ' ol-searchgps';
-    options.placeholder = options.placeholder || 'lon,lat';
-
+    options.className = (options.className || '') + ' ol-searchcoord';
+    options.placeholder = options.placeholder || 'x,y';
+    
     super(options);
-
+    
+    // Projection
+    this.projection_ = options.projection || 'EPSG:3857'
     // Geolocation
     this.geolocation = new ol_Geolocation({
       projection: "EPSG:4326",
@@ -51,25 +54,23 @@ var ol_control_SearchGPS = class olcontrolSearchGPS extends ol_control_Search {
       }.bind(this)
     });
 
-    // DMS switcher
-    ol_ext_element.createSwitch({
-      html: 'decimal',
-      after: 'DMS',
-      change: function (e) {
-        if (e.target.checked)
-          this.element.classList.add('ol-dms');
-        else
-          this.element.classList.remove('ol-dms');
-      }.bind(this),
-      parent: this.element
-    });
-
     this._createForm();
 
     // Move list to the end
     var ul = this.element.querySelector("ul.autocomplete");
     this.element.appendChild(ul);
-
+  }
+  /** Set the projection
+   * 
+   */
+  setProjection(proj) {
+    if (this.projection_ !== proj) {
+      this.projection_ = proj;
+      this.clearHistory();
+      this.element.querySelectorAll('INPUT[type="number"]').forEach(function(i) {
+        i.value = '';
+      })
+    }
   }
   /** Create input form
    * @private
@@ -78,33 +79,16 @@ var ol_control_SearchGPS = class olcontrolSearchGPS extends ol_control_Search {
 
     // Value has change
     var onchange = function (e) {
-      if (e.target.classList.contains('ol-dms')) {
-        lon.value = (lond.value < 0 ? -1 : 1) * Number(lond.value) + Number(lonm.value) / 60 + Number(lons.value) / 3600;
-        lon.value = (lond.value < 0 ? -1 : 1) * Math.round(lon.value * 10000000) / 10000000;
-        lat.value = (latd.value < 0 ? -1 : 1) * Number(latd.value) + Number(latm.value) / 60 + Number(lats.value) / 3600;
-        lat.value = (latd.value < 0 ? -1 : 1) * Math.round(lat.value * 10000000) / 10000000;
-      }
       if (lon.value || lat.value) {
         this._input.value = lon.value + ',' + lat.value;
       } else {
         this._input.value = '';
       }
-      if (!e.target.classList.contains('ol-dms')) {
-        var s = ol_coordinate_toStringHDMS([Number(lon.value), Number(lat.value)]);
-        var c = s.replace(/(N|S|E|W)/g, '').split('″');
-        c[1] = c[1].trim().split(' ');
-        lond.value = (/W/.test(s) ? -1 : 1) * parseInt(c[1][0]);
-        lonm.value = parseInt(c[1][1]);
-        lons.value = parseInt(c[1][2]);
-        c[0] = c[0].trim().split(' ');
-        latd.value = (/W/.test(s) ? -1 : 1) * parseInt(c[0][0]);
-        latm.value = parseInt(c[0][1]);
-        lats.value = parseInt(c[0][2]);
-      }
+      // Center on coords
       this.search();
     }.bind(this);
 
-    function createInput(className, unit) {
+    function createInput(className) {
       var input = ol_ext_element.create('INPUT', {
         className: className,
         type: 'number',
@@ -115,43 +99,30 @@ var ol_control_SearchGPS = class olcontrolSearchGPS extends ol_control_Search {
           'change keyup': onchange
         }
       });
-      if (unit) {
-        ol_ext_element.create('SPAN', {
-          className: 'ol-dms',
-          html: unit,
-          parent: div,
-        });
-      }
       return input;
     }
 
-    // Longitude
+    // X
     var div = ol_ext_element.create('DIV', {
       className: 'ol-longitude',
       parent: this.element
     });
     ol_ext_element.create('LABEL', {
-      html: 'Longitude',
+      html: 'X',
       parent: div
     });
     var lon = createInput('ol-decimal');
-    var lond = createInput('ol-dms', '°');
-    var lonm = createInput('ol-dms', '\'');
-    var lons = createInput('ol-dms', '"');
 
-    // Latitude
+    // Y
     div = ol_ext_element.create('DIV', {
       className: 'ol-latitude',
       parent: this.element
     });
     ol_ext_element.create('LABEL', {
-      html: 'Latitude',
+      html: 'Y',
       parent: div
     });
     var lat = createInput('ol-decimal');
-    var latd = createInput('ol-dms', '°');
-    var latm = createInput('ol-dms', '\'');
-    var lats = createInput('ol-dms', '"');
 
     // Focus on open
     if (this.button) {
@@ -170,6 +141,8 @@ var ol_control_SearchGPS = class olcontrolSearchGPS extends ol_control_Search {
     this.geolocation.on('change', function () {
       this.geolocation.setTracking(false);
       var coord = this.geolocation.getPosition();
+      coord = ol_proj_transform(coord, 'EPSG:4326', this.projection_)
+      console.log(coord)
       lon.value = coord[0];
       lat.value = coord[1];
       this._triggerCustomEvent('keyup', lon);
@@ -185,15 +158,11 @@ var ol_control_SearchGPS = class olcontrolSearchGPS extends ol_control_Search {
     var c = s.split(',');
     c[0] = Number(c[0]);
     c[1] = Number(c[1]);
-    // Name
-    s = ol_coordinate_toStringHDMS(c);
-    if (s)
-      s = s.replace(/(°|′|″) /g, '$1');
     // 
-    var coord = ol_proj_transform([c[0], c[1]], 'EPSG:4326', this.getMap().getView().getProjection());
+    var coord = ol_proj_transform([c[0], c[1]], this.projection_, this.getMap().getView().getProjection());
     result.push({ gps: c, coordinate: coord, name: s });
     return result;
   }
 }
 
-export default ol_control_SearchGPS
+export default ol_control_SearchCoordinates
