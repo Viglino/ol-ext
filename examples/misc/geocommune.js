54ac2514c558228faf271516b9efb265e5ab648e
viglino <jean-marc.viglino@ign.fr>
2026-06-24 17:02:09 +0200

    [UPD] carte commune

--- examples/misc/geocommune.js
+++ examples/misc/geocommune.js
@@ -45,8 +45,13 @@ async function getCommune(insee) {
   const extent = commune.getGeometry().getExtent();
   const reponse = await fetch('commune.carte');
   const carte = await reponse.json();
-  carte.layers.forEach(l => {
+  carte.param.lon = (extent[0] + extent[2]) / 2;
+  carte.param.lat = (extent[1] + extent[3]) / 2;
+  for (let i=0; i<carte.layers.length; i++) {
+    l = carte.layers[i];
+  // carte.layers.forEach(l => {
     if (l.title === 'Commune') {
+      // Change communes attributes
       const geojson = new ol.format.GeoJSON().writeFeatureObject(commune, {
         featureProjection: 'EPSG:4326',
         dataProjection: map.getView().getProjection()
@@ -54,25 +59,62 @@ async function getCommune(insee) {
       l.features[0].attributes = geojson.properties;
       l.features[0].type = geojson.geometry.type;
       l.features[0].coords = geojson.geometry.coordinates;
-      l.crop.coordinates[0] = geojson.geometry.coordinates;
-      console.log('COMMUNE', l, geojson);
-    }
-    if (l.url) {
-      const filter = l.url.split('%2C');
-      const index = filter.findIndex(f => /BBOX/.test(f));
-      console.log('BBOX', l.title, index);
-      if (index>=0) {
-        filter[index+1] = extent[0];
-        filter[index+2] = extent[1];
-        filter[index+3] = extent[2];
-        filter[index+4] = extent[3];
-        // filter[index+5] = 'EPSG%3A4326';
-        l.url = filter.join('%2C');
+      if (geojson.geometry.type === 'Polygon') {
+        l.crop.coordinates[0] = geojson.geometry.coordinates;
+      } else {
+        l.crop.coordinates = geojson.geometry.coordinates;
       }
     }
-  })
-  console.log(carte);
-  saveAs(new Blob([JSON.stringify(carte, null, 2)], {type: "application/json"}), `commune-${insee}.carte`);
+    if (l.url) {
+      l.url = l.url.split('?');
+      const search = l.url.pop().split('&');
+      search.forEach((s,i) => {
+        if (/BBOX/.test(s)) {
+          let filter = s.split(',');
+          const index = filter.findIndex(f => /BBOX/.test(f));
+          if (index>=0) {
+            filter[index+1] = extent[0];
+            filter[index+2] = extent[1];
+            filter[index+3] = extent[2];
+            filter[index+4] = extent[3];
+            // filter[index+5] = 'EPSG%3A4326';
+            search[i] = filter.join(',');
+          }
+        }
+      });
+      l.url.push(search.join('&'));
+      l.url = l.url.join('?');
+      // Get points layer
+      const player = carte.layers.find(l2 => l.title === l2.title && l2 !== l);
+      if (player) {
+        await fetch(l.url).then(r => r.json()).then(json => {
+          const features = (new ol.format.GeoJSON()).readFeatures(json, {
+            dataProjection: 'EPSG:4326',
+            featureProjection: map.getView().getProjection()
+          });
+          features.forEach(f => {
+            if (f.getGeometry().getType() === 'Polygon') {
+              f.setGeometry(f.getGeometry().getInteriorPoint());
+            } else if (f.getGeometry().getType() === 'MultiPolygon') {
+              f.setGeometry(f.getGeometry().getPolygons()[0].getInteriorPoint());
+            }
+          });
+          const pts = (new ol.format.GeoJSON()).writeFeaturesObject(features);
+          player.features = [];
+          pts.features.forEach(f => {
+            player.features.push({
+              attributes: f.properties,
+              type: f.geometry.type,
+              coords: f.geometry.coordinates
+            });
+          });
+          console.log(json, player, features);
+
+        });
+      }
+    }
+  }
+  saveAs(new Blob([JSON.stringify(carte, null, ' ')], {type: "application/json"}), `commune-${insee}.carte`);
 }
 dlog.show();
 
